55125ef23ef8ffd9a62954ea39d9a6f281a0218a
Javi <gatufo@gmail.com>
2015-04-20 13:49:35 +0200

    Added Rule and CallableRule

---
+++ spidermon/rules.py
@@ -0,0 +1,40 @@
+from .context import create_context_dict
+
+
+class Rule(object):
+    """
+    Base class for rules
+    """
+    __rule_type__ = 'rule'
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    @property
+    def type(self):
+        return self.__rule_type__
+
+    @property
+    def name(self):
+        return self.type
+
+    def check(self, stats):
+        raise NotImplementedError
+
+
+class CallableRule(Rule):
+    """
+    Callable rule. must be initialiazed with a callable that will be used for checking
+    """
+    __rule_type__ = 'callable'
+
+    def __init__(self, call):
+        self.call = call
+
+    @property
+    def name(self):
+        return self.call.func_name
+
+    def check(self, stats):
+        context_dict = create_context_dict(stats)
+        return self.call(**context_dict)
---
+++ tests/test_rules.py
@@ -0,0 +1,61 @@
+import pytest
+import unittest
+
+from spidermon import Rule, CallableRule
+from spidermon.context import create_context_dict
+
+
+STATS_01 = {
+    'scraped_items': 100,
+}
+
+STATS_02 = {
+    'scraped_items': 0,
+}
+
+STATS_EMPTY = {}
+
+
+class DummyRule(Rule):
+    def check(self, stats):
+        return True
+
+
+class SimpleRule(Rule):
+    def check(self, stats):
+        return stats.scraped_items == 100
+
+
+def function_rule(stats):
+    return stats.scraped_items == 100
+
+
+class RulesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.context_stats_01 = create_context_dict(STATS_01)
+        self.context_stats_02 = create_context_dict(STATS_01)
+        self.context_stats_empty = create_context_dict(STATS_EMPTY)
+
+    def test_base_rule(self):
+        rule = Rule()
+        with pytest.raises(NotImplementedError):
+            rule.check(**self.context_stats_empty)
+
+    def test_dummy_rule(self):
+        rule = DummyRule()
+        assert rule.check(**self.context_stats_empty) is True
+
+    def test_simple_rule(self):
+        rule = SimpleRule()
+        assert rule.check(**self.context_stats_01) is True
+        assert rule.check(**self.context_stats_02) is True
+        with pytest.raises(AttributeError):
+            rule.check(**self.context_stats_empty)
+
+    def test_function_rule(self):
+        rule = CallableRule(function_rule)
+        assert rule.check(STATS_01) is True
+        assert rule.check(STATS_02) is False
+        with pytest.raises(AttributeError):
+            rule.check(STATS_EMPTY)
