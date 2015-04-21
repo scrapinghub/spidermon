efc004aa2b655d2a02c84cf52b6aff8a81d27600
gatufo <gatufo@gmail.com>
2015-04-21 21:11:46 +0200

    Added test fixtures

---
+++ tests/fixtures/stats.py
@@ -0,0 +1,19 @@
+STATS_A = {
+    'item_scraped_count': 150,
+}
+
+STATS_B = {
+    'item_scraped_count': 0,
+}
+
+STATS_EMPTY = {}
+
+STATS_TO_EVALUATE = {
+    'downloader/exception_count': 16,
+    'downloader/response_count': 63785,
+    'finish_reason': 'finished',
+    'item_scraped_count': 29836,
+    'has_errors': True,
+    'has_redirections': True,
+}
+
