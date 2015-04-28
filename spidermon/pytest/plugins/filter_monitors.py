97de136879dca90c449ef03ef24a268fc8635163
Javi <gatufo@gmail.com>
2015-04-28 16:14:45 +0200

    Fixed bug in pytest plugin

--- spidermon/pytest/plugins/filter_monitors.py
+++ spidermon/pytest/plugins/filter_monitors.py
@@ -4,10 +4,10 @@ from spidermon import Monitor
 
 
 def pytest_report_header(config):
-    return "Spidermon test filtering"
+    return "Spidermon monitor filtering"
 
 
 @pytest.mark.trylast
 def pytest_collection_modifyitems(session, config, items):
     items[:] = [item for item in items
-                if not (item.cls and issubclass(item.cls, Monitor))]
+                if hasattr(item, 'cls') and not (item.cls and issubclass(item.cls, Monitor))]
