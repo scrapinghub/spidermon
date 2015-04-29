e0fb7a909e0fb901d9feb6f35f0d7d8c6ee90f2e
Javi <gatufo@gmail.com>
2015-04-29 15:01:37 +0200

    Added naming tests

---
+++ tests/fixtures/naming.py
@@ -0,0 +1,65 @@
+from spidermon import monitors, Monitor, MonitorSuite
+
+
+# ----------------------------------
+# Monitors
+# ----------------------------------
+class UnnamedMonitor(Monitor):
+    def test_without_name(self):
+        pass
+
+    @monitors.name('A Test')
+    def test_with_name(self):
+        pass
+
+
+@monitors.name('Class Monitor')
+class NamedMonitor(Monitor):
+    def test_without_name(self):
+        pass
+
+    @monitors.name('A Test')
+    def test_with_name(self):
+        pass
+
+
+# ----------------------------------
+# Child Suites
+# ----------------------------------
+class BaseChildSuite(MonitorSuite):
+    monitors = [
+        UnnamedMonitor,
+        NamedMonitor,
+        ('Instance Monitor', UnnamedMonitor),
+        ('Instance Monitor', NamedMonitor),
+    ]
+
+
+class ChildUnnamedSuite(BaseChildSuite):
+    pass
+
+
+@monitors.name('The Child Suite')
+class ChildNamedSuite(BaseChildSuite):
+    pass
+
+
+# ----------------------------------
+# Top Suites
+# ----------------------------------
+class BaseTopSuite(MonitorSuite):
+    monitors = [
+        ChildUnnamedSuite,
+        ChildNamedSuite,
+        ('Instance Suite Name', ChildUnnamedSuite),
+        ('Instance Suite Name', ChildNamedSuite),
+    ]
+
+
+class UnnamedTopSuite(BaseTopSuite):
+    pass
+
+
+@monitors.name('The Top Suite')
+class NamedTopSuite(BaseTopSuite):
+    pass
