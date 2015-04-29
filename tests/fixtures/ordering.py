ba30269b7e47a9c6aa04b1dc7474276e095d5248
Javi <gatufo@gmail.com>
2015-04-29 20:40:55 +0200

    Added ordering tests

---
+++ tests/fixtures/ordering.py
@@ -0,0 +1,84 @@
+from spidermon import Monitor, MonitorSuite, monitors
+
+
+class DummyMonitor(Monitor):
+    def runTest(self):
+        pass
+
+
+class DummyMonitorSuite(MonitorSuite):
+    monitors = [DummyMonitor]
+
+
+class Unordered:
+    class A(DummyMonitor):
+        pass
+
+    class B(DummyMonitor):
+        pass
+
+    class C(DummyMonitorSuite):
+        pass
+
+    class D(DummyMonitorSuite):
+        pass
+
+
+class Ordered:
+    @monitors.order(1)
+    class A(DummyMonitor):
+        pass
+
+    @monitors.order(2)
+    class B(DummyMonitor):
+        pass
+
+    @monitors.order(3)
+    class C(DummyMonitorSuite):
+        pass
+
+    @monitors.order(4)
+    class D(DummyMonitorSuite):
+        pass
+
+
+class UnorderedMethodsMonitor(Monitor):
+
+    def test_a(self):
+        pass
+
+    def test_b(self):
+        pass
+
+    def test_c(self):
+        pass
+
+
+class OrderedMethodsMonitor(Monitor):
+
+    @monitors.order(3)
+    def test_a(self):
+        pass
+
+    @monitors.order(2)
+    def test_b(self):
+        pass
+
+    @monitors.order(1)
+    def test_c(self):
+        pass
+
+
+class EqualOrderedMethodsMonitor(Monitor):
+
+    @monitors.order(5)
+    def test_a(self):
+        pass
+
+    @monitors.order(5)
+    def test_b(self):
+        pass
+
+    @monitors.order(5)
+    def test_c(self):
+        pass
---
+++ tests/test_ordering.py
@@ -0,0 +1,61 @@
+from fixtures.ordering import *
+
+SUITES_SEQUENCES = [
+    # ------------------------------------------------------------------------------------------------------------
+    # monitor sequence                                      expected sequence
+    # ------------------------------------------------------------------------------------------------------------
+
+    # Unordered: should follow same insertion order (FIFO)
+    ([Unordered.A, Unordered.B],                            [Unordered.A, Unordered.B]),
+    ([Unordered.B, Unordered.A],                            [Unordered.B, Unordered.A]),
+    ([Unordered.C, Unordered.D],                            [Unordered.C, Unordered.D]),
+    ([Unordered.D, Unordered.C],                            [Unordered.D, Unordered.C]),
+    ([Unordered.A, Unordered.B, Unordered.C, Unordered.D],  [Unordered.A, Unordered.B, Unordered.C, Unordered.D]),
+    ([Unordered.D, Unordered.C, Unordered.B, Unordered.A],  [Unordered.D, Unordered.C, Unordered.B, Unordered.A]),
+    ([Unordered.C, Unordered.B, Unordered.A, Unordered.D],  [Unordered.C, Unordered.B, Unordered.A, Unordered.D]),
+    ([Unordered.A, Unordered.A, Unordered.B, Unordered.B],  [Unordered.A, Unordered.A, Unordered.B, Unordered.B]),
+    ([Unordered.C, Unordered.C, Unordered.B, Unordered.B],  [Unordered.C, Unordered.C, Unordered.B, Unordered.B]),
+
+    # Ordered: should follow decorators order (A, B, C, D)
+    ([Ordered.A, Ordered.B],                                [Ordered.A, Ordered.B]),
+    ([Ordered.B, Ordered.A],                                [Ordered.A, Ordered.B]),
+    ([Ordered.C, Ordered.D],                                [Ordered.C, Ordered.D]),
+    ([Ordered.D, Ordered.C],                                [Ordered.C, Ordered.D]),
+    ([Ordered.A, Ordered.B, Ordered.C, Ordered.D],          [Ordered.A, Ordered.B, Ordered.C, Ordered.D]),
+    ([Ordered.D, Ordered.C, Ordered.B, Ordered.A],          [Ordered.A, Ordered.B, Ordered.C, Ordered.D]),
+    ([Ordered.C, Ordered.B, Ordered.A, Ordered.D],          [Ordered.A, Ordered.B, Ordered.C, Ordered.D]),
+    ([Ordered.A, Ordered.A, Ordered.B, Ordered.B],          [Ordered.A, Ordered.A, Ordered.B, Ordered.B]),
+    ([Ordered.C, Ordered.C, Ordered.B, Ordered.B],          [Ordered.B, Ordered.B, Ordered.C, Ordered.C]),
+]
+
+METHOD_SEQUENCES = [
+    # ------------------------------------------------------------------------------------------------------------
+    # monitor class                 expected methods sequence
+    # ------------------------------------------------------------------------------------------------------------
+    (UnorderedMethodsMonitor,       ['test_a', 'test_b', 'test_c']),
+    (OrderedMethodsMonitor,         ['test_c', 'test_b', 'test_a']),
+    (EqualOrderedMethodsMonitor,    ['test_a', 'test_b', 'test_c']),
+]
+
+
+def test_suite_ordering():
+    for monitors_sequence, expected_sequence in SUITES_SEQUENCES:
+        suite = MonitorSuite()
+        suite.add_monitors(monitors_sequence)
+        sequence = [_extract_test_class(t) for t in suite]
+        assert sequence == expected_sequence
+
+
+def test_method_ordering():
+    for monitor_class, expected_sequence in METHOD_SEQUENCES:
+        suite = MonitorSuite()
+        suite.add_monitor(monitor_class)
+        sequence = [t.method_name for t in suite.all_tests]
+        assert sequence == expected_sequence
+
+
+def _extract_test_class(suite):
+    if suite.__class__ == MonitorSuite:
+        return _extract_test_class(suite._tests[0])
+    else:
+        return suite.__class__
