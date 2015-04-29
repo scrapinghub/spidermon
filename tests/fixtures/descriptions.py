c1dc58c0e74d8544e1352088fc18cb90fa3b455c
Javi <gatufo@gmail.com>
2015-04-29 19:40:42 +0200

    Added descriptions tests

---
+++ tests/fixtures/descriptions.py
@@ -0,0 +1,71 @@
+from spidermon import Monitor, MonitorSuite, monitors
+
+
+# ----------------------------------
+# Monitors
+# ----------------------------------
+class BaseMonitor(Monitor):
+    def runTest(self):
+        pass
+
+
+class NoDescriptedMonitor(BaseMonitor):
+    pass
+
+
+class DocstringDescriptedMonitor(BaseMonitor):
+    """docstring monitor description"""
+    pass
+
+
+@monitors.description('decorator monitor description')
+class DescoratedDescriptedMonitor(BaseMonitor):
+    pass
+
+
+@monitors.description('decorator monitor description')
+class DescoratedDescriptedMonitor2(BaseMonitor):
+    """docstring monitor description"""
+
+
+# ----------------------------------
+# Suites
+# ----------------------------------
+class NoDescriptedSuite(MonitorSuite):
+    pass
+
+
+class DocstringDescriptedSuite(MonitorSuite):
+    """docstring suite description"""
+    pass
+
+
+@monitors.description('decorator suite description')
+class DescoratedDescriptedSuite(MonitorSuite):
+    pass
+
+
+@monitors.description('decorator suite description')
+class DescoratedDescriptedSuite2(MonitorSuite):
+    """docstring suite description"""
+
+
+# ----------------------------------
+# Methods
+# ----------------------------------
+class DescriptedMethodsMonitor(Monitor):
+
+    def test_not_descripted(self):
+        pass
+
+    def test_docstring_descripted(self):
+        """docstring method description"""
+        pass
+
+    @monitors.description('decorator method description')
+    def test_decorator_descripted(self):
+        pass
+
+    @monitors.description('decorator method description')
+    def test_decorator_descripted2(self):
+        """docstring method description"""
---
+++ tests/test_descriptions.py
@@ -0,0 +1,46 @@
+from fixtures.descriptions import *
+
+MONITOR_DESCRIPTIONS = [
+    # --------------------------------------------------------------
+    # monitor class                 description
+    # --------------------------------------------------------------
+    (NoDescriptedMonitor,           ''),
+    (DocstringDescriptedMonitor,    'docstring monitor description'),
+    (DescoratedDescriptedMonitor,   'decorator monitor description'),
+    (DescoratedDescriptedMonitor2,  'decorator monitor description'),
+]
+
+SUITE_DESCRIPTIONS = [
+    # --------------------------------------------------------------
+    # suite class                   description
+    # --------------------------------------------------------------
+    (NoDescriptedSuite,             ''),
+    (DocstringDescriptedSuite,      'docstring suite description'),
+    (DescoratedDescriptedSuite,     'decorator suite description'),
+    (DescoratedDescriptedSuite2,    'decorator suite description'),
+]
+
+METHOD_DESCRIPTIONS = [
+    # --------------------------------------------------------------
+    # monitor class                   description
+    # --------------------------------------------------------------
+    (DescriptedMethodsMonitor,      'test_not_descripted',          ''),
+    (DescriptedMethodsMonitor,      'test_docstring_descripted',    'docstring method description'),
+    (DescriptedMethodsMonitor,      'test_decorator_descripted',    'decorator method description'),
+    (DescriptedMethodsMonitor,      'test_decorator_descripted2',   'decorator method description'),
+]
+
+
+def test_monitor_descriptions():
+    for monitor_class, description in MONITOR_DESCRIPTIONS:
+        assert monitor_class().monitor_description == description
+
+
+def test_suite_descriptions():
+    for suite_class, description in SUITE_DESCRIPTIONS:
+        assert suite_class().description == description
+
+
+def test_method_descriptions():
+    for monitor_class, method_name, description in METHOD_DESCRIPTIONS:
+        assert monitor_class(method_name).method_description == description
