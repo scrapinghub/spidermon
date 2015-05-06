e98a5d435f94061fe85ae5c4e6389e803d4f7aff
Javi <gatufo@gmail.com>
2015-05-06 19:44:32 +0200

    Added include_package_data=True to setup.py to include jinja templates

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'spidermon',
-    version      = '0.1',
-    packages     = find_packages(),
-    zip_safe     = False,
+    name = 'spidermon',
+    version = '0.1',
+    packages = find_packages(),
+    zip_safe = False,
+    include_package_data = True,
     install_requires=[
         'six==1.9.0',
     ],
