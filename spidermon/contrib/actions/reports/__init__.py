a72586356816b9025335f7f50e630ebe832e0ba8
Javi <gatufo@gmail.com>
2015-05-08 13:47:44 +0200

    Added GenerateFileReport action

--- spidermon/contrib/actions/reports/__init__.py
+++ spidermon/contrib/actions/reports/__init__.py
@@ -2,11 +2,11 @@ from spidermon.contrib.actions.templates import ActionWithTemplates
 from spidermon.exceptions import NotConfigured
 
 
-class BaseReport(ActionWithTemplates):
+class GenerateReport(ActionWithTemplates):
     template = None
 
     def __init__(self, template):
-        super(BaseReport, self).__init__()
+        super(GenerateReport, self).__init__()
         self.template = template or self.template
         self.report = ''
         if not self.template:
@@ -14,13 +14,22 @@ class BaseReport(ActionWithTemplates):
 
     @classmethod
     def from_crawler_kwargs(cls, crawler):
-        return {
+        kwargs = super(GenerateReport, cls).from_crawler_kwargs(crawler)
+        kwargs.update({
             'template': crawler.settings.get('SPIDERMON_REPORT_TEMPLATE'),
-        }
+        })
+        return kwargs
 
     def run_action(self):
+        self.before_render_report()
         self.render_report()
-        print self.report
+        self.after_render_report()
+
+    def before_render_report(self):
+        pass
 
     def render_report(self):
         self.report = self.render_template(self.template)
+
+    def after_render_report(self):
+        pass
---
+++ spidermon/contrib/actions/reports/file.py
@@ -0,0 +1,26 @@
+from spidermon.exceptions import NotConfigured
+
+from . import GenerateReport
+
+
+class GenerateFileReport(GenerateReport):
+    template = None
+    filename = None
+
+    def __init__(self, template, filename):
+        super(GenerateFileReport, self).__init__(template=template)
+        self.filename = filename or self.filename
+        if not self.filename:
+            raise NotConfigured("You must define a template output file.")
+
+    @classmethod
+    def from_crawler_kwargs(cls, crawler):
+        kwargs = super(GenerateFileReport, cls).from_crawler_kwargs(crawler)
+        kwargs.update({
+            'filename': crawler.settings.get('SPIDERMON_REPORT_FILENAME'),
+        })
+        return kwargs
+
+    def after_render_report(self):
+        with open(self.filename, "w") as f:
+            f.write(self.report)
