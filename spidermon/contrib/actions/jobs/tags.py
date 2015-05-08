0cbf4679d87d3218d40f922d7f0e628f0947f535
Javi <gatufo@gmail.com>
2015-05-08 12:18:35 +0200

    Added RemoveJobTags action

--- spidermon/contrib/actions/jobs/tags.py
+++ spidermon/contrib/actions/jobs/tags.py
@@ -1,24 +1,47 @@
 from spidermon.core.actions import Action
 
 
-class AddJobTags(Action):
+class JobTagsAction(Action):
     tags = []
+    tag_settings = None
 
     def __init__(self, tags=None):
-        super(AddJobTags, self).__init__()
+        super(JobTagsAction, self).__init__()
         tags = tags or self.tags
         self.tags = tags if isinstance(tags, list) else [tags]
 
     @classmethod
     def from_crawler_kwargs(cls, crawler):
-        return {
-            'tags': crawler.settings.get('SPIDERMON_JOB_TAGS'),
-        }
+        kwargs = super(JobTagsAction, cls).from_crawler_kwargs()
+        if cls.tag_settings:
+            kwargs.update({
+                'tags': crawler.settings.get(cls.tag_settings),
+            })
+        return kwargs
 
     def run_action(self):
         if self.data.hubstorage.available and self.tags:
             job_metadata = self.data.hubstorage.job.metadata
-            for tag in self.tags:
-                if tag not in job_metadata['tags']:
-                    job_metadata['tags'].append(tag)
+            self.process_tags(job_metadata)
             job_metadata.save()
+
+    def process_tags(self, job_metadata):
+        raise NotImplementedError
+
+
+class AddJobTags(JobTagsAction):
+    tag_settings = 'SPIDERMON_JOB_TAGS_TO_ADD'
+
+    def process_tags(self, job_metadata):
+        for tag in self.tags:
+            if tag not in job_metadata['tags']:
+                job_metadata['tags'].append(tag)
+
+
+class RemoveJobTags(JobTagsAction):
+    tag_settings = 'SPIDERMON_JOB_TAGS_TO_REMOVE'
+
+    def process_tags(self, job_metadata):
+        for tag in self.tags:
+            if tag in job_metadata['tags']:
+                job_metadata['tags'].remove(tag)
