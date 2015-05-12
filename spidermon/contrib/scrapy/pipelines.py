3e2452f6005508c38cef35051481100321d46149
Javi <gatufo@gmail.com>
2015-05-12 20:11:44 +0200

    Added scrapy item validation pipeline

---
+++ spidermon/contrib/scrapy/pipelines.py
@@ -0,0 +1,91 @@
+import six
+import json
+from collections import defaultdict
+
+from scrapy.exceptions import DropItem, NotConfigured
+from scrapy.utils.misc import load_object
+from scrapy import Field
+
+from spidermon.contrib.validation import SchematicsValidator, JSONSchemaValidator
+from schematics.models import Model
+
+from .stats import ValidationStatsManager
+
+
+DEFAULT_ERRORS_FIELD = '_validation'
+DEFAULT_ADD_ERRORS_TO_ITEM = False
+
+
+class ItemValidationPipeline(object):
+
+    def __init__(self, validators, stats, add_errors_to_item=False, errors_field=None):
+        self.add_errors_to_item = add_errors_to_item
+        self.errors_field = errors_field or DEFAULT_ERRORS_FIELD
+        self.validators = []
+        self.stats = ValidationStatsManager(stats)
+        for validator in validators:
+            self._add_validator(validator)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        validators = []
+        validators += [cls._load_jsonschema_validator(v)
+                       for v in crawler.settings.getlist('SPIDERMON_VALIDATION_SCHEMAS')]
+        validators += [cls._load_schematics_validator(v)
+                       for v in crawler.settings.getlist('SPIDERMON_VALIDATION_MODELS')]
+        return cls(
+            validators=validators,
+            stats=crawler.stats,
+            add_errors_to_item=crawler.settings.get('SPIDERMON_VALIDATION_ADD_ERRORS_TO_ITEM'),
+            errors_field=crawler.settings.get('SPIDERMON_VALIDATION_ERRORS_FIELD'),
+        )
+
+    @classmethod
+    def _load_jsonschema_validator(cls, schema):
+        if isinstance(schema, six.string_types):
+            if '.json' in schema:
+                with open(schema, 'r') as f:
+                    schema = json.load(f)
+            else:
+                schema = load_object(schema)
+                if isinstance(schema, six.string_types):
+                    schema = json.loads(schema)
+        if not isinstance(schema, dict):
+            raise NotConfigured('Invalid schema, jsonschemas must either be defined as:\n'
+                                '- an object path to a python dict.\n'
+                                '- an object path to a JSON string.\n'
+                                '- a path to a JSON file.')
+        return JSONSchemaValidator(schema)
+
+    @classmethod
+    def _load_schematics_validator(cls, model_path):
+        model_class = load_object(model_path)
+        if not issubclass(model_class, Model):
+            raise NotConfigured('Invalid model, models must subclass schematics.models.Model')
+        return SchematicsValidator(model_class)
+
+    def process_item(self, item, _):
+        data = dict(item)
+        self.stats.add_item()
+        self.stats.add_fields(len(data.keys()))
+        for validator in self.validators:
+            ok, errors = validator.validate(data)
+            if not ok:
+                for field_name, messages in errors.items():
+                    for message in messages:
+                        self.stats.add_field_error(field_name, message)
+                if self.add_errors_to_item:
+                    self._add_errors_to_item(item, errors)
+        return item
+
+    def _add_validator(self, validator):
+        self.validators.append(validator)
+        self.stats.add_validator(validator.name)
+
+    def _add_errors_to_item(self, item, errors):
+        if not self.errors_field in item.__class__.fields:
+            item.__class__.fields[self.errors_field] = Field()
+        if not self.errors_field in item._values:
+            item[self.errors_field] = defaultdict(list)
+        for field_name, messages in errors.items():
+            item[self.errors_field][field_name] += messages
---
+++ spidermon/contrib/scrapy/stats.py
@@ -0,0 +1,44 @@
+from slugify import Slugify
+
+
+DEFAULT_PREFIX = 'spidermon/validation'
+
+
+class NAMES:
+    ITEMS = 'items'
+    FIELDS = 'fields'
+    ERRORS = 'errors'
+    VALIDATORS = 'validators'
+
+
+class ValidationStatsManager(object):
+    slugifier = Slugify(to_lower=True, separator='_')
+
+    def __init__(self, stats, prefix=None, slugify=True):
+        self.stats = stats
+        self.prefix = prefix or DEFAULT_PREFIX
+        self.slugify = slugify
+
+    def add_validator(self, class_name):
+        self.stats.inc_value(self._get_stats_name(NAMES.VALIDATORS))
+        self.stats.set_value(self._get_stats_name(NAMES.VALIDATORS, class_name), True)
+
+    def add_field_error(self, field, error):
+        self.stats.inc_value(self._get_stats_name(NAMES.FIELDS, NAMES.ERRORS))
+        self.stats.inc_value(self._get_stats_name(NAMES.FIELDS, NAMES.ERRORS, error))
+        self.stats.inc_value(self._get_stats_name(NAMES.FIELDS, NAMES.ERRORS, error, field))
+
+    def add_fields(self, count):
+        self.stats.inc_value(self._get_stats_name(NAMES.FIELDS), count=count)
+
+    def add_item(self):
+        self.stats.inc_value(self._get_stats_name(NAMES.ITEMS))
+
+    def add_item_with_errors(self):
+        self.stats.inc_value(self._get_stats_name(NAMES.ITEMS, NAMES.ERRORS))
+
+    def _get_stats_name(self, *names):
+        return '/'.join([self.prefix] + list([self._get_name(n) for n in names]))
+
+    def _get_name(self, name):
+        return self.slugifier(name) if self.slugify else name
