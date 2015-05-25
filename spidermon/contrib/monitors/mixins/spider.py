069734aa58d6b47f3dcdd00898e8bacea9bbc822
Javi <gatufo@gmail.com>
2015-05-25 15:38:05 +0200

    Added status_codes counters to SpiderMonitorMixin

--- spidermon/contrib/monitors/mixins/spider.py
+++ spidermon/contrib/monitors/mixins/spider.py
@@ -1,7 +1,99 @@
 from spidermon.exceptions import NotConfigured
+from spidermon.contrib.stats.counters import PercentCounter
 
 from .stats import StatsMonitorMixin
 from .job import JobMonitorMixin
+
+
+DOWNLOADER_RESPONSE_COUNT = 'downloader/response_count'
+DOWNLOADER_RESPONSE_STATUS = 'downloader/response_status_count/'
+DOWNLOADER_STATUS_CODES_INFORMATIONAL = ['1\d{2}$']
+DOWNLOADER_STATUS_CODES_SUCCESSFUL = ['2\d{2}$']
+DOWNLOADER_STATUS_CODES_REDIRECTIONS = ['3\d{2}']
+DOWNLOADER_STATUS_CODES_BAD_REQUESTS = ['4\d{2}']
+DOWNLOADER_STATUS_CODES_INTERNAL_SERVER_ERRORS = ['5\d{2}']
+DOWNLOADER_STATUS_CODES_OTHERS = ['[^1-5].*']
+DOWNLOADER_STATUS_CODES_ERRORS = DOWNLOADER_STATUS_CODES_BAD_REQUESTS + DOWNLOADER_STATUS_CODES_INTERNAL_SERVER_ERRORS
+
+
+from spidermon.contrib.stats.analyzer import StatsAnalyzer
+from spidermon.contrib.stats.counters import DictPercentCounter
+
+
+class StatusCodesInfo(object):
+    def __init__(self, status_codes_count, analyzer):
+
+        self._count = status_codes_count
+        self.analyzer = analyzer
+
+        self.successful = DictPercentCounter(total=status_codes_count)
+        self.redirections = DictPercentCounter(total=status_codes_count)
+        self.bad_requests = DictPercentCounter(total=status_codes_count)
+        self.internal_server_errors = DictPercentCounter(total=status_codes_count)
+        self.errors = DictPercentCounter(total=status_codes_count)
+        self.others = DictPercentCounter(total=status_codes_count)
+
+        # all status codes
+        self.all = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=None, target=self.all)
+
+        # 1xx. informational
+        self.informational = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_INFORMATIONAL, target=self.informational)
+
+        # 2xx. successful
+        self.successful = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_SUCCESSFUL, target=self.successful)
+
+        # 3xx. redirections
+        self.redirections = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_REDIRECTIONS, target=self.redirections)
+
+        # 4xx. bad requests
+        self.bad_requests = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_BAD_REQUESTS, target=self.bad_requests)
+
+        # 5xx. internal server errors
+        self.internal_server_errors = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_INTERNAL_SERVER_ERRORS, target=self.internal_server_errors)
+
+        # >= 6xx. others
+        self.others = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_OTHERS, target=self.others)
+
+        # >= errors (4xx + 5xx)
+        self.errors = DictPercentCounter(total=status_codes_count)
+        self._add_status_codes(pattern=DOWNLOADER_STATUS_CODES_ERRORS, target=self.errors)
+
+    def _add_status_codes(self, pattern, target):
+        for code, counter in self._get_response_codes(pattern).items():
+            target.add_value(code, counter.count)
+
+    def _get_response_codes(self, codes=None):
+        codes = codes or ['[^/]+']
+        return_codes = {}
+        for code in codes:
+            return_codes.update(self._get_response_code(code))
+        return return_codes
+
+    def _get_response_code(self, code):
+        return dict([(code, PercentCounter(count, self._count))
+                     for count, code in
+                     self.analyzer.search(
+                         pattern=DOWNLOADER_RESPONSE_STATUS + ('(%s)$' % code),
+                         include_matches=True,
+                         ).values()
+        ])
+
+
+class ResponsesInfo(object):
+    def __init__(self, stats):
+        self.analyzer = StatsAnalyzer(stats=stats)
+        self.count = self.analyzer.search(DOWNLOADER_RESPONSE_COUNT + '$').get(DOWNLOADER_RESPONSE_COUNT, 0)
+        self.codes = StatusCodesInfo(
+            status_codes_count=self.count,
+            analyzer=self.analyzer,
+        )
 
 
 class SpiderMonitorMixin(StatsMonitorMixin, JobMonitorMixin):
@@ -16,3 +108,9 @@ class SpiderMonitorMixin(StatsMonitorMixin, JobMonitorMixin):
         if 'spider' not in self.data:
             raise NotConfigured('Spider not available!')
         return self.data.spider
+
+    @property
+    def responses(self):
+        if not hasattr(self, '_responses'):
+            self._responses = ResponsesInfo(self.stats)
+        return self._responses
