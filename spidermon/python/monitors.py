69aed8212f78fbefb156f47c2d22b822d1db9fb8
Javi <gatufo@gmail.com>
2015-06-18 13:40:26 +0200

    Updated ExpressionsMonitor

--- spidermon/python/monitors.py
+++ spidermon/python/monitors.py
@@ -1,13 +1,14 @@
-from spidermon.contrib.monitors.mixins import ValidationMonitorMixin
+from spidermon.contrib.monitors.mixins import ValidationMonitorMixin, SpiderMonitorMixin
 
 from .factory import PythonExpressionsMonitor
 
 
-class ExpressionsMonitor(PythonExpressionsMonitor, ValidationMonitorMixin):
+class ExpressionsMonitor(PythonExpressionsMonitor, ValidationMonitorMixin, SpiderMonitorMixin):
     def get_context_data(self):
         context = super(ExpressionsMonitor, self).get_context_data()
         context.update({
             'stats': self.stats,
             'validation': self.validation,
+            'responses': self.responses,
         })
         return context
