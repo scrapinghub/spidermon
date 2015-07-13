f59edf9763b165dfdc2646bedab4c4d070b197b0
gatufo <gatufo@gmail.com>
2015-07-13 20:03:46 +0200

    Moved url secret to s3 filename

--- spidermon/contrib/actions/reports/s3.py
+++ spidermon/contrib/actions/reports/s3.py
@@ -105,13 +105,15 @@ class CreateS3Report(CreateReport):
         )
 
     def get_s3_filename(self):
-        return self.render_text_template(self.s3_filename)
+        return '{secret}/{filename}'.format(
+            secret=self.get_url_secret(),
+            filename=self.render_text_template(self.s3_filename),
+        )
 
     def get_s3_report_url(self):
-        return 'http://{bucket}.{region}/reports/{secret}/{filename}'.format(
+        return 'http://{bucket}.{region}/reports/{filename}'.format(
             bucket=self.s3_bucket,
             region=self.s3_region_endpoint,
-            secret=self.get_url_secret(),
             filename=self.get_s3_filename(),
         )
 
