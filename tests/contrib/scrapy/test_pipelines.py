df9c9178d331e9d1316986de9b4f73dd6fd57442
Jose Ricardo <zehzinho@gmail.com>
2015-10-26 16:39:44 -0300

    Add test for the ItemValidationPipeline

---
+++ tests/contrib/scrapy/test_pipelines.py
@@ -0,0 +1,36 @@
+from spidermon.contrib.scrapy.pipelines import ItemValidationPipeline
+from scrapy import Item, Field
+from scrapy.statscollectors import MemoryStatsCollector
+from scrapy.spiders import Spider
+from scrapy.utils.test import get_crawler
+from spidermon.contrib.validation import JSONSchemaValidator
+
+
+class Tree(Item):
+    child = Field()
+
+
+def test_can_process_items_with_nested_items():
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema",
+        "required": [
+            "child",
+        ],
+        "type": "object",
+        "properties": {
+            "child": {
+                "type": "object",
+            }
+        }
+    }
+
+    stats = MemoryStatsCollector(get_crawler(Spider))
+    json_schema_validator = JSONSchemaValidator(schema)
+
+    item = Tree()
+    item['child'] = Tree()
+
+    pipeline = ItemValidationPipeline([json_schema_validator], stats)
+    pipeline.process_item(item, None)
+
+    assert 'spidermon/validation/items/errors' not in stats.get_stats()
