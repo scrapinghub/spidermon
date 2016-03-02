c813df15aa3bc2caaf994d775c56ddea9f6a166c
Victoria Terenina (torymur) <torymur@gmail.com>
2016-03-02 22:03:39 +0000

    extend context of expressions monitor
    - add support for crawler, spider and job components
    - safer way to work with NotConfigured elements: presents Context obj,
    which keeps track of not-configured elements and throws exception
    only in eval time instead of building context time

--- spidermon/python/monitors.py
+++ spidermon/python/monitors.py
@@ -1,14 +1,23 @@
-from spidermon.contrib.monitors.mixins import ValidationMonitorMixin, SpiderMonitorMixin
+from spidermon.contrib.monitors.mixins import (ValidationMonitorMixin,
+                                               SpiderMonitorMixin)
 
 from .factory import PythonExpressionsMonitor
+from .context import Context
 
 
-class ExpressionsMonitor(PythonExpressionsMonitor, ValidationMonitorMixin, SpiderMonitorMixin):
+class ExpressionsMonitor(PythonExpressionsMonitor,
+                         ValidationMonitorMixin,
+                         SpiderMonitorMixin):
     def get_context_data(self):
-        context = super(ExpressionsMonitor, self).get_context_data()
-        context.update({
-            'stats': self.stats,
-            'validation': self.validation,
-            'responses': self.responses,
-        })
+        context = Context()
+        attrs = [
+            'stats',
+            'crawler',
+            'spider',
+            'job',
+            'validation',
+            'responses'
+        ]
+        context.extend_via_attrs(self, attrs)
         return context
+
