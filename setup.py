cd6e079d4f8644abaf329ed51933fa05be76f761
Victoria Terenina (torymur) <torymur@gmail.com>
2016-03-28 13:31:00 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'spidermon',
-    version = '0.1.1',
+    version = '0.1.2',
     packages = find_packages(),
     package_data={'spidermon': ['VERSION']},
     zip_safe = False,
