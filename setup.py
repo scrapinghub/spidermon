ab7c866d2147aea83f8e332fe668097e68bc1b85
Jesús Losada <dev@jesuslosada.com>
2018-02-21 16:15:45 +0000

    Fix inconsistent whitespace in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'spidermon',
-    version = '0.1.2',
-    packages = find_packages(),
+    name='spidermon',
+    version='0.1.2',
+    packages=find_packages(),
     package_data={'spidermon': ['VERSION']},
-    zip_safe = False,
-    include_package_data = True,
+    zip_safe=False,
+    include_package_data=True,
     install_requires=[
         'six>=1.9.0',
     ],
