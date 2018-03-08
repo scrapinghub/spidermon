e409f7994f50bd389096c5c96667d2af52f86339
Jesús Losada <dev@jesuslosada.com>
2018-03-08 16:16:47 +0000

    Bump version: 0.1.2 → 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='spidermon',
-    version='0.1.2',
+    version='1.0.0',
     packages=find_packages(),
     package_data={'spidermon': ['VERSION']},
     zip_safe=False,
