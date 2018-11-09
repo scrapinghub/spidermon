ac8db919e6f9828431ce32c61489fa7e174a56df
Jesús Losada <dev@jesuslosada.com>
2018-11-09 15:40:37 +0100

    Bump version: 1.5.0 → 1.6.0

--- spidermon/__init__.py
+++ spidermon/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = '1.5.0' 
+__version__ = '1.6.0' 
 
 from .core.monitors import Monitor
 from .core.suites import MonitorSuite
