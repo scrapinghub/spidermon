e870a4221e1ff06496b43152e203bf14c0e66950
Renne Rocha <renne@scrapinghub.com>
2018-12-21 12:04:08 -0200

    Improved monitor docs

--- examples/tutorial/tutorial/monitors.py
+++ examples/tutorial/tutorial/monitors.py
@@ -7,14 +7,13 @@ from spidermon.contrib.monitors.mixins import StatsMonitorMixin
 @monitors.name('Item count')
 class ItemCountMonitor(Monitor):
 
-    @monitors.name('Minimum number of items')
-    def test_minimum_number_of_items(self):
+    @monitors.name('Minimum items extracted')
+    def test_minimum_number_of_items_extracted(self):
+        minimum_threshold = 1000
         item_extracted = getattr(self.data.stats, 'item_scraped_count', 0)
-        minimum_threshold = 10
-
-        msg = 'Extracted less than {} items'.format(minimum_threshold)
-        self.assertTrue(
-            item_extracted >= minimum_threshold, msg=msg
+        self.assertFalse(
+            item_extracted < minimum_threshold,
+            msg='Extracted less than {} items'.format(minimum_threshold)
         )
 
 
