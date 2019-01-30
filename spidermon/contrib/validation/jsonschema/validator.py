54e88a7ba42ad3e615f1ee562ba3fffc8f3388b4
Renne Rocha <renne@scrapinghub.com>
2019-01-30 17:02:25 -0200

    Included argument to indicate desired JSON Schema draft in validator

--- spidermon/contrib/validation/jsonschema/validator.py
+++ spidermon/contrib/validation/jsonschema/validator.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import re
 
-from jsonschema.validators import Draft4Validator
+from jsonschema.validators import validators as jsonschema_validators
 
 from spidermon.contrib.validation.validator import Validator
 
@@ -14,26 +14,34 @@ REQUIRED_RE = re.compile("'(.+)' is a required property")
 
 class JSONSchemaValidator(Validator):
     default_translator = JSONSchemaMessageTranslator()
+    default_jsonschema_version = 'draft4'
     name = 'JSONSchema'
 
-    def __init__(self, schema, translator=None, use_default_translator=True):
+    def __init__(self, schema, version='draft4', translator=None, use_default_translator=True):
         super(JSONSchemaValidator, self).__init__(
             translator=translator,
             use_default_translator=use_default_translator,
         )
         self._schema = schema
+        self._version = version
 
     def _validate(self, data, strict=False):
-        validator = Draft4Validator(
+        default_validator = jsonschema_validators.get(self.default_jsonschema_version)
+
+        validator_cls = jsonschema_validators.get(self._version)
+        if validator_cls is None:
+            validator_cls = default_validator
+
+        validator = validator_cls(
             schema=self._schema,
             format_checker=format_checker,
         )
         errors = validator.iter_errors(data)
-        for e in errors:
-            #print e
-            absolute_path = list(e.absolute_path)
-            required_match = REQUIRED_RE.search(e.message)
+
+        for error in errors:
+            absolute_path = list(error.absolute_path)
+            required_match = REQUIRED_RE.search(error.message)
             if required_match:
                 absolute_path.append(required_match.group(1))
             field_name = '.'.join([str(p) for p in absolute_path])
-            self._add_errors({field_name: [e.message]})
+            self._add_errors({field_name: [error.message]})
