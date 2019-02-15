3afc6c53b0e38078a094e4521919cff7560a6e58
Raphael Passini <raphapassini@gmail.com>
2019-02-15 18:17:31 -0200

    Implement improvements in basic monitors after code review

--- spidermon/contrib/scrapy/monitors.py
+++ spidermon/contrib/scrapy/monitors.py
@@ -1,4 +1,3 @@
-# tutorial/monitors.py
 from spidermon import monitors, MonitorSuite, Monitor
 from spidermon.exceptions import NotConfigured
 from ..monitors.mixins.spider import SpiderMonitorMixin
@@ -6,7 +5,6 @@ from ..monitors.mixins.spider import SpiderMonitorMixin
 SPIDERMON_MIN_ITEMS = 'SPIDERMON_MIN_ITEMS'
 SPIDERMON_MAX_ERROR = 'SPIDERMON_MAX_ITEMS'
 SPIDERMON_EXPECTED_FINISH_REASONS = 'SPIDERMON_EXPECTED_FINISH_REASONS'
-SPIDERMON_MAX_UNWANTED_HTTP_CODES = 'SPIDERMON_MAX_UNWANTED_HTTP_CODES'
 SPIDERMON_UNWANTED_HTTP_CODES = 'SPIDERMON_UNWANTED_HTTP_CODES'
 
 
@@ -14,10 +12,9 @@ class BaseScrapyMonitor(Monitor, SpiderMonitorMixin):
     longMessage = False
 
     def get_settings(self, name, default=None):
-        """ Return a settings prioritized by:
+        """Return a settings prioritized by:
 
-            #. ``{name}`` as a spider attribute if is present
-            #. ``{name}.upper()`` in the ``crawler.settings`` object
+            #. ``{name}`` in the ``crawler.settings`` object
             #. If ``crawler.settings.{name}`` is a dict, try to get the
                ``{spider.name}`` key of that dictionary, this allows you to
                have a different configuration per spider in a settings level,
@@ -34,39 +31,38 @@ class BaseScrapyMonitor(Monitor, SpiderMonitorMixin):
         settings_value = self.crawler.settings.get(name.upper())
         if type(settings_value) == dict:
             settings_value = settings_value.get(self.spider.name, None)
-        spider_value = getattr(self.spider, name.lower(), None)
-        return spider_value or settings_value or default
+        return settings_value or default
 
 
 @monitors.name('Extracted Items Monitor')
 class ItemCountMonitor(BaseScrapyMonitor):
-    """ Check for the minimum number of extracted items.
+    """Check if spider extracted the minimum number of items.
     You can configure it using ``SPIDERMON_MIN_ITEMS`` setting.
     There's **NO** default value for this setting, if you try to use this
-    monitor without setting it it'll raise a ``NotConfigured`` exception
+    monitor without setting it, it'll raise a ``NotConfigured`` exception.
     """
+    def run(self, result):
+        self.minimum_threshold = self.get_settings(SPIDERMON_MIN_ITEMS)
+        if not self.minimum_threshold:
+            raise NotConfigured('You should specify a minimum number of items '
+                                'to check against.')
+        return super().run(result)
 
     @monitors.name('Should extract the minimum amount of items')
     def test_minimum_number_of_items(self):
-        minimum_threshold = self.get_settings(SPIDERMON_MIN_ITEMS)
-        if not minimum_threshold:
-            raise NotConfigured('You should specify a minimum number of items '
-                                'to check against.')
-
         item_extracted = getattr(self.stats, 'item_scraped_count', 0)
         msg = 'Extracted {} items, the expected minimum is {}'.format(
-            item_extracted, minimum_threshold)
+            item_extracted, self.minimum_threshold)
         self.assertTrue(
-            item_extracted >= minimum_threshold, msg=msg
+            item_extracted >= self.minimum_threshold, msg=msg
         )
 
 
 @monitors.name('Log Monitor')
 class LogMonitor(BaseScrapyMonitor):
-    """ Check for errors in the spider log you can configure the expected
-    number of ERROR log messages using ``SPIDERMON_MAX_ERROR``,
-    the default is ``0``."""
-
+    """Check for errors in the spider log.
+    You can configure the expected number of ERROR log messages using
+    ``SPIDERMON_MAX_ERROR``. The default is ``0``."""
     @monitors.name('Should not have any errors')
     def test_should_not_have_errors(self):
         errors_threshold = self.get_settings(SPIDERMON_MAX_ERROR, 0)
@@ -78,13 +74,12 @@ class LogMonitor(BaseScrapyMonitor):
 
 @monitors.name('Finish Reason Monitor')
 class FinishReasonMonitor(BaseScrapyMonitor):
-    """ Check if a job has a expected finish reason.
+    """Check if a job has a expected finish reason.
     You can configure the expected reason with the
     ``SPIDERMON_EXPECTED_FINISH_REASONS``, it should be an ``iterable`` of
     valid finish reasons.
 
-    The default value of this settings is: ``['finished', ]``"""
-
+    The default value of this settings is: ``['finished', ]``."""
     @monitors.name('Should have the expected finished reason(s)')
     def test_should_not_have_errors(self):
         expected_reasons = self.get_settings(
@@ -96,50 +91,56 @@ class FinishReasonMonitor(BaseScrapyMonitor):
 
 
 @monitors.name('Check for unwanted http status codes')
-class UnwantedHttpStatus(BaseScrapyMonitor):
-    """ Check for maximum number of unwanted http codes.
+class UnwantedHTTPCodesMonitor(BaseScrapyMonitor):
+    """Check for maximum number of unwanted HTTP codes.
 
-    You can configure a list of unwanted http codes with
-    ``SPIDERMON_UNWANTED_HTTP_CODES`` the default value is:
-    ``[400, 407, 429, 500, 502, 503, 504, 523, 540, 541]``
+    You can configure a ``dict`` of unwanted HTTP codes with
+    ``SPIDERMON_UNWANTED_HTTP_CODES`` the default value is::
 
-    You can also configured the **maximum** number of times that those
-    response codes with ``SPIDERMON_MAX_UNWANTED_HTTP_CODES`` the default
-    value is ``1``.
+        DEFAULT_ERROR_CODES = {
+            code: 10
+            for code in [400, 407, 429, 500, 502, 503, 504, 523, 540, 541]}
+
+    **WARNING**: You can have this settings by spider like::
+
+    ``SPIDERMON_UNWANTED_HTTP_CODES = {'my_spider': {400: 2, 500: 100}}``
+
+    in order to do that overwrite the ``custom_settings`` property of the
+    Spider.
     """
-    default_codes = [400, 407, 429, 500, 502, 503, 504, 523, 540, 541]
+    DEFAULT_ERROR_CODES = {
+        code: 10
+        for code in [400, 407, 429, 500, 502, 503, 504, 523, 540, 541]}
 
     @monitors.name('Should not hit the limit of unwanted http status')
     def test_check_unwanted_http_codes(self):
-        max_errors = self.get_settings(SPIDERMON_MAX_UNWANTED_HTTP_CODES, 1)
-        error_codes = self.get_settings(SPIDERMON_UNWANTED_HTTP_CODES,
-                                        self.default_codes)
-        for code in error_codes:
-            times = self.stats.get(
+        error_codes = self.crawler.settings.get(
+            SPIDERMON_UNWANTED_HTTP_CODES, self.DEFAULT_ERROR_CODES)
+        for code, max_errors in error_codes.items():
+            count = self.stats.get(
                 'downloader/response_status_count/{}'.format(code), 0)
             msg = 'Found {} Responses with status code={} - '\
-                  'This exceed the limit of {}'.format(times, code, max_errors)
-            self.assertTrue(times < max_errors, msg=msg)
+                  'This exceed the limit of {}'.format(count, code, max_errors)
+            self.assertTrue(count <= max_errors, msg=msg)
 
 
 class SpiderCloseMonitorSuite(MonitorSuite):
-    """ Implemented the following monitors:
+    """This Monitor Suite implements the following monitors:
 
     * :class:`ItemCountMonitor`
     * :class:`LogMonitor`
     * :class:`FinishReasonMonitor`
-    * :class:`UnwantedHttpStatus`
+    * :class:`UnwantedHTTPCodesMonitor`
 
     You can easily enable this monitor *after* enabling Spidermon::
 
             SPIDERMON_SPIDER_CLOSE_MONITORS = (
                 'spidermon.contrib.scrapy.monitors.SpiderCloseMonitorSuite',
             )
-
     """
     monitors = [
         ItemCountMonitor,
         LogMonitor,
         FinishReasonMonitor,
-        UnwantedHttpStatus,
+        UnwantedHTTPCodesMonitor,
     ]
--- tests/test_monitors.py
+++ tests/test_monitors.py
@@ -1,184 +1,153 @@
+import pytest
+
 from scrapy import Spider
 from scrapy.crawler import Crawler
 from spidermon.contrib.scrapy.runners import SpiderMonitorRunner
 from spidermon.contrib.scrapy.monitors import (
-    FinishReasonMonitor, ItemCountMonitor, LogMonitor, UnwantedHttpStatus,
+    FinishReasonMonitor, ItemCountMonitor, LogMonitor,
+    UnwantedHTTPCodesMonitor,
     SPIDERMON_MIN_ITEMS, SPIDERMON_EXPECTED_FINISH_REASONS,
-    SPIDERMON_MAX_ERROR, SPIDERMON_MAX_UNWANTED_HTTP_CODES)
+    SPIDERMON_MAX_ERROR, SPIDERMON_UNWANTED_HTTP_CODES)
 from spidermon import MonitorSuite
+from spidermon.exceptions import NotConfigured
 
 
-def new_crawler():
-    return Crawler(Spider)
+@pytest.fixture
+def make_data(request):
+    def _make_data(settings=None):
+        crawler = Crawler(Spider, settings=settings)
+        spider = Spider('dummy')
+        return {
+            'stats': crawler.stats.get_stats(),
+            'crawler': crawler,
+            'spider': spider,
+            'runner': SpiderMonitorRunner(spider=spider),
+            'job': None,
+        }
+    return _make_data
+
+
+@pytest.fixture
+def item_count_suite():
+    return MonitorSuite(monitors=[ItemCountMonitor, ])
 
 
 def new_suite(monitors):
     return MonitorSuite(monitors=monitors)
 
 
-def test_needs_to_configure_item_count_monitor():
-    """ Should raise an exception when ItemCountMonitor it's not configured """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    suite = new_suite([ItemCountMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('item_scraped_count', 10)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
-    runner.run(suite, **data)
-    for r in runner.result.monitor_results:
-        assert('NotConfigured' in r.error)
+def test_needs_to_configure_item_count_monitor(make_data, item_count_suite):
+    """Should raise an exception when ItemCountMonitor it's not configured """
+    data = make_data()
+    runner = data.pop('runner')
+    data['crawler'].stats.set_value('item_scraped_count', 10)
+    with pytest.raises(NotConfigured):
+        runner.run(item_count_suite, **data)
 
 
-def test_item_count_monitor_should_fail():
-    """ ItemCount should fail when the desired # of items is not extracted """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    setattr(spider, SPIDERMON_MIN_ITEMS.lower(), 100)
-    suite = new_suite([ItemCountMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('item_scraped_count', 10)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
-    runner.run(suite, **data)
+def test_item_count_monitor_should_fail(make_data, item_count_suite):
+    """ItemCount should fail when the desired # of items is not extracted """
+    data = make_data({SPIDERMON_MIN_ITEMS: 100})
+    runner = data.pop('runner')
+    data['stats']['item_scraped_count'] = 10
+    runner.run(item_count_suite, **data)
     for r in runner.result.monitor_results:
         assert('Extracted 10 items, the expected minimum is 100' in r.error)
 
 
-def test_item_count_monitor_should_pass():
-    """ ItemCount should pass when extract the desired amount of items """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    setattr(spider, SPIDERMON_MIN_ITEMS.lower(), 100)
-    suite = new_suite([ItemCountMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('item_scraped_count', 100)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
-    runner.run(suite, **data)
+def test_item_count_monitor_extracted_exact_items(make_data, item_count_suite):
+    """ItemCount should pass when extract the exact amount of items """
+    data = make_data({'SPIDERMON_MIN_ITEMS': 100})
+    runner = data.pop('runner')
+    data['stats']['item_scraped_count'] = 100
+    runner.run(item_count_suite, **data)
     assert(runner.result.monitor_results[0].error is None)
 
 
-def test_finished_reason_monitor_should_fail():
+def test_item_count_monitor_extracted_more_than_expected(
+        make_data, item_count_suite):
+    """ItemCount should pass when extract more than expected amount """
+    data = make_data({'SPIDERMON_MIN_ITEMS': 100})
+    runner = data.pop('runner')
+    data['stats']['item_scraped_count'] = 500
+    runner.run(item_count_suite, **data)
+    assert(runner.result.monitor_results[0].error is None)
+
+
+def test_item_count_monitor_extracted_less_than_expected(
+        make_data, item_count_suite):
+    """ItemCount should fail when extract less than expected amount """
+    data = make_data({'SPIDERMON_MIN_ITEMS': 100})
+    runner = data.pop('runner')
+    data['stats']['item_scraped_count'] = 50
+    runner.run(item_count_suite, **data)
+    assert('Extracted 50 items, the expected minimum is 100' in
+           runner.result.monitor_results[0].error)
+
+
+def test_finished_reason_monitor_should_fail(make_data):
     """ FinishedReason should fail when spider finished with unexpected
     reason """
-    crawler = new_crawler()
-    spider = Spider('dummy')
+    data = make_data()
+    runner = data.pop('runner')
     suite = new_suite([FinishReasonMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('finish_reason', 'bad_finish')
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
+    data['stats']['finish_reason'] = 'bad_finish'
     runner.run(suite, **data)
     assert('Finished with "bad_finish" the expected reasons' in
            runner.result.monitor_results[0].error)
 
 
-def test_finished_reason_monitor_should_pass():
+def test_finished_reason_monitor_should_pass(make_data):
     """ FinishedReason should succeed when spider finished with expected
     reason """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    setattr(spider, SPIDERMON_EXPECTED_FINISH_REASONS.lower(),
-            ('special_reason', ))
+    data = make_data({SPIDERMON_EXPECTED_FINISH_REASONS: 'special_reason'})
+    runner = data.pop('runner')
     suite = new_suite([FinishReasonMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('finish_reason', 'special_reason')
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
+    data['stats']['finish_reason'] = 'special_reason'
     runner.run(suite, **data)
     assert(runner.result.monitor_results[0].error is None)
 
 
-def test_log_monitor_should_fail():
+def test_log_monitor_should_fail(make_data):
     """ Log should fail if the # of error log messages exceed the limit """
-    crawler = new_crawler()
-    spider = Spider('dummy')
+    data = make_data()
+    runner = data.pop('runner')
     suite = new_suite([LogMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('log_count/ERROR', 2)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
+    data['stats']['log_count/ERROR'] = 2
     runner.run(suite, **data)
-    assert(' Found 2 errors in log' in runner.result.monitor_results[0].error)
+    assert('Found 2 errors in log' in runner.result.monitor_results[0].error)
 
 
-def test_log_monitor_should_pass():
+def test_log_monitor_should_pass(make_data):
     """ Log should pass if the # of error log message DOES NOT
     exceed the limit """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    setattr(spider, SPIDERMON_MAX_ERROR.lower(), 50)
+    data = make_data({SPIDERMON_MAX_ERROR: 50})
+    runner = data.pop('runner')
     suite = new_suite([LogMonitor, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('log_count/ERROR', 2)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
+    data['stats']['log_count/ERROR'] = 2
     runner.run(suite, **data)
     assert(runner.result.monitor_results[0].error is None)
 
 
-def test_unwanted_httpcodes_should_fail():
+def test_unwanted_httpcodes_should_fail(make_data):
     """Unwanted HTTP Code should fail if # off responses with error status
     codes is higher than expected """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    suite = new_suite([UnwantedHttpStatus, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('downloader/response_status_count/500', 1)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
+    data = make_data()
+    runner = data.pop('runner')
+    suite = new_suite([UnwantedHTTPCodesMonitor, ])
+    data['stats']['downloader/response_status_count/500'] = 11
     runner.run(suite, **data)
-    assert('Found 1 Responses with status code=500'
+    assert('Found 11 Responses with status code=500'
            in runner.result.monitor_results[0].error)
 
 
-def test_unwanted_httpcodes_should_pass():
+def test_unwanted_httpcodes_should_pass(make_data):
     """Unwanted HTTP Code should pass if # off responses with error status
     codes is lower than expected """
-    crawler = new_crawler()
-    spider = Spider('dummy')
-    setattr(spider, SPIDERMON_MAX_UNWANTED_HTTP_CODES.lower(), 100)
-    suite = new_suite([UnwantedHttpStatus, ])
-    runner = SpiderMonitorRunner(spider=spider)
-    crawler.stats.set_value('downloader/response_status_count/500', 99)
-    data = {
-            'stats': crawler.stats.get_stats(),
-            'crawler': crawler,
-            'spider': spider,
-            'job': None,
-    }
+    data = make_data({SPIDERMON_UNWANTED_HTTP_CODES: {500: 10, 400: 2}})
+    runner = data.pop('runner')
+    suite = new_suite([UnwantedHTTPCodesMonitor, ])
+    data['stats']['downloader/response_status_count/500'] = 8
+    data['stats']['downloader/response_status_count/400'] = 2
     runner.run(suite, **data)
     assert(runner.result.monitor_results[0].error is None)
