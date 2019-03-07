be9501938a2a2935e685f326e9a2f25b66818fba
Renne Rocha <renne@scrapinghub.com>
2019-03-07 16:48:17 -0300

    Use pytest-mock instead of builtin library

--- tests/test_spidermon_signal_connect.py
+++ tests/test_spidermon_signal_connect.py
@@ -14,34 +14,35 @@ def get_crawler(settings_dict=None):
 
 
 @pytest.mark.skipif(sys.version_info < (3, 4), reason="requires python3.4 or higher")
-def test_spider_opened_connect_signal():
-    from unittest import mock
+def test_spider_opened_connect_signal(mocker):
 
-    with mock.patch.object(Spidermon, "spider_opened") as spider_opened_method:
-        crawler = get_crawler(
-            settings_dict={
-                "SPIDERMON_ENABLED": True,
-                "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
-            }
-        )
-        spider = Spider.from_crawler(crawler, "example.com")
-        crawler.signals.send_catch_log(signal=signals.spider_opened, spider=spider)
-        assert spider_opened_method.called, "spider_opened not called"
+    spider_opened_method = mocker.patch.object(Spidermon, "spider_opened")
+
+    crawler = get_crawler(
+        settings_dict={
+            "SPIDERMON_ENABLED": True,
+            "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
+        }
+    )
+    spider = Spider.from_crawler(crawler, "example.com")
+    crawler.signals.send_catch_log(signal=signals.spider_opened, spider=spider)
+
+    assert spider_opened_method.called, "spider_opened not called"
 
 
 @pytest.mark.skipif(sys.version_info < (3, 4), reason="requires python3.4 or higher")
-def test_spider_closed_connect_signal():
-    from unittest import mock
+def test_spider_closed_connect_signal(mocker):
+    spider_closed_method = mocker.patch.object(Spidermon, "spider_closed")
 
-    with mock.patch.object(Spidermon, "spider_closed") as spider_closed_method:
-        crawler = get_crawler(
-            settings_dict={
-                "SPIDERMON_ENABLED": True,
-                "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
-            }
-        )
-        spider = Spider.from_crawler(crawler, "example.com")
-        crawler.signals.send_catch_log(
-            signal=signals.spider_closed, spider=spider, reason=None
-        )
-        assert spider_closed_method.called, "spider_closed not called"
+    crawler = get_crawler(
+        settings_dict={
+            "SPIDERMON_ENABLED": True,
+            "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
+        }
+    )
+    spider = Spider.from_crawler(crawler, "example.com")
+    crawler.signals.send_catch_log(
+        signal=signals.spider_closed, spider=spider, reason=None
+    )
+
+    assert spider_closed_method.called, "spider_closed not called"
