b9ff8fa282b2c9eaf0320a6e4d944d82366a80a5
Luiz Silva <luizfrdasilva@gmail.com>
2019-03-07 12:21:18 -0300

    Rename fixture for better readability

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -10,7 +10,7 @@ from spidermon.contrib.scrapy.extensions import Spidermon
 
 
 @pytest.fixture
-def crawler():
+def get_crawler():
     def _crawler(extended_settings={}):
         settings = {
             "SPIDERMON_ENABLED": True,
@@ -29,9 +29,9 @@ def suites():
     return ["tests.fixtures.suites.Suite01"]
 
 
-def test_spider_opened_suites_should_run(crawler, suites):
+def test_spider_opened_suites_should_run(get_crawler, suites):
     """The suites defined at spider_opened_suites should be loaded and run """
-    crawler = crawler()
+    crawler = get_crawler()
     spidermon = Spidermon(crawler, spider_opened_suites=suites)
     spidermon.spider_opened_suites[0].run = mock.MagicMock()
     spidermon.spider_opened(crawler.spider)
@@ -39,9 +39,9 @@ def test_spider_opened_suites_should_run(crawler, suites):
     assert spidermon.spider_opened_suites[0].run.called
 
 
-def test_spider_closed_suites_should_run(crawler, suites):
+def test_spider_closed_suites_should_run(get_crawler, suites):
     """The suites defined at spider_closed_suites should be loaded and run """
-    crawler = crawler()
+    crawler = get_crawler()
     spidermon = Spidermon(
         crawler, spider_opened_suites=suites, spider_closed_suites=suites
     )
@@ -52,9 +52,9 @@ def test_spider_closed_suites_should_run(crawler, suites):
     assert spidermon.spider_closed_suites[0].run.called
 
 
-def test_engine_stopped_suites_should_run(crawler, suites):
+def test_engine_stopped_suites_should_run(get_crawler, suites):
     """The suites defined at engine_stopped_suites should be loaded and run """
-    crawler = crawler()
+    crawler = get_crawler()
     spidermon = Spidermon(crawler, engine_stopped_suites=suites)
     spidermon.engine_stopped_suites[0].run = mock.MagicMock()
     spidermon.engine_stopped()
@@ -62,30 +62,30 @@ def test_engine_stopped_suites_should_run(crawler, suites):
     assert spidermon.engine_stopped_suites[0].run.called
 
 
-def test_spider_opened_suites_should_run_from_signal(crawler, suites):
+def test_spider_opened_suites_should_run_from_signal(get_crawler, suites):
     """The suites defined at SPIDERMON_SPIDER_OPEN_MONITORS setting should be loaded and run """
     settings = {"SPIDERMON_SPIDER_OPEN_MONITORS": suites}
-    crawler = crawler(settings)
+    crawler = get_crawler(settings)
     spidermon = Spidermon.from_crawler(crawler)
     spidermon.spider_opened_suites[0].run = mock.MagicMock()
     crawler.signals.send_catch_log(signal=signals.spider_opened, spider=crawler.spider)
     assert spidermon.spider_opened_suites[0].run.called
 
 
-def test_spider_closed_suites_should_run_from_signal(crawler, suites):
+def test_spider_closed_suites_should_run_from_signal(get_crawler, suites):
     """The suites defined at SPIDERMON_SPIDER_CLOSE_MONITORS setting should be loaded and run """
     settings = {"SPIDERMON_SPIDER_CLOSE_MONITORS": suites}
-    crawler = crawler(settings)
+    crawler = get_crawler(settings)
     spidermon = Spidermon.from_crawler(crawler)
     spidermon.spider_closed_suites[0].run = mock.MagicMock()
     crawler.signals.send_catch_log(signal=signals.spider_closed, spider=crawler.spider)
     assert spidermon.spider_closed_suites[0].run.called
 
 
-def test_engine_stopped_suites_should_run_from_signal(crawler, suites):
+def test_engine_stopped_suites_should_run_from_signal(get_crawler, suites):
     """The suites defined at SPIDERMON_ENGINE_STOP_MONITORS setting should be loaded and run """
     settings = {"SPIDERMON_ENGINE_STOP_MONITORS": suites}
-    crawler = crawler(settings)
+    crawler = get_crawler(settings)
     spidermon = Spidermon.from_crawler(crawler)
     spidermon.engine_stopped_suites[0].run = mock.MagicMock()
     crawler.signals.send_catch_log(signal=signals.engine_stopped, spider=crawler.spider)
