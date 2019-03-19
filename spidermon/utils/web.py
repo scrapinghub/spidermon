b3986271a6cfac8206bc3459eb1f61e9428ccc46
manycoding <manycoding@users.noreply.github.com>
2019-03-19 14:48:09 -0300

    A bit simple

--- spidermon/utils/web.py
+++ spidermon/utils/web.py
@@ -7,9 +7,7 @@ from six.moves.urllib.request import urlopen
 def is_schema_url(path):
     result = urlparse(path)
     try:
-        if all([result.scheme, result.netloc, result.path]):
-            return True
-        return False
+        return all([result.scheme, result.netloc, result.path])
     except AttributeError:
         return False
 
