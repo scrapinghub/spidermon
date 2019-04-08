548faf54db1c5d35c49b2bbf29ee7c1a7c6a667e
Valery M <manycoding@users.noreply.github.com>
2019-04-08 16:54:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     package_data={"spidermon": ["VERSION"]},
     zip_safe=False,
     include_package_data=True,
-    install_requires=["jsonschema", "python-slugify", "six>=1.9.0"],
+    install_requires=["jsonschema[format]", "python-slugify", "six>=1.9.0"],
     tests_require=test_requirements,
     extras_require={
         # Specific monitors and tools to support notifications and reports
