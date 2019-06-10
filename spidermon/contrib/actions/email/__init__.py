13bc266f8d4ea231a4eccdfed692058542584856
rosheen33 <rosheen33@gmail.com>
2019-06-10 17:16:47 +0500

    added default template setting

--- spidermon/contrib/actions/email/__init__.py
+++ spidermon/contrib/actions/email/__init__.py
@@ -22,7 +22,8 @@ class SendEmail(ActionWithTemplates):
     body_text = None
     body_text_template = None
     body_html = None
-    body_html_template = None
+    # Default value for template added
+    body_html_template = 'reports/email/monitors/result.jinja'
     fake = False
 
     def __init__(
