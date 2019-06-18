08c2c5077d6ee05d0afa3aac0233de66567467f9
Renne Rocha <renne@scrapinghub.com>
2019-06-18 14:48:55 -0300

    Removed duplicated code and use existing Scrapy test util function to create test crawlers

--- tests/test_spidermon_signal_connect.py
+++ tests/test_spidermon_signal_connect.py
@@ -2,27 +2,25 @@ import sys
 
 import pytest
 from scrapy import signals
-from scrapy.crawler import CrawlerRunner
 from scrapy.spiders import Spider
+from scrapy.utils.test import get_crawler
 
 from spidermon.contrib.scrapy.extensions import Spidermon
 
 
-def get_crawler(settings_dict=None):
-    runner = CrawlerRunner(settings_dict)
-    return runner.create_crawler(Spider)
+@pytest.fixture
+def spidermon_enabled_settings():
+    return {
+        "SPIDERMON_ENABLED": True,
+        "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
+    }
 
 
 @pytest.mark.skipif(sys.version_info < (3, 4), reason="requires python3.4 or higher")
-def test_spider_opened_connect_signal(mocker):
+def test_spider_opened_connect_signal(mocker, spidermon_enabled_settings):
     spider_opened_method = mocker.patch.object(Spidermon, "spider_opened")
 
-    crawler = get_crawler(
-        settings_dict={
-            "SPIDERMON_ENABLED": True,
-            "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
-        }
-    )
+    crawler = get_crawler(settings_dict=spidermon_enabled_settings)
     spider = Spider.from_crawler(crawler, "example.com")
     crawler.signals.send_catch_log(signal=signals.spider_opened, spider=spider)
 
@@ -30,15 +28,10 @@ def test_spider_opened_connect_signal(mocker):
 
 
 @pytest.mark.skipif(sys.version_info < (3, 4), reason="requires python3.4 or higher")
-def test_spider_closed_connect_signal(mocker):
+def test_spider_closed_connect_signal(mocker, spidermon_enabled_settings):
     spider_closed_method = mocker.patch.object(Spidermon, "spider_closed")
 
-    crawler = get_crawler(
-        settings_dict={
-            "SPIDERMON_ENABLED": True,
-            "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
-        }
-    )
+    crawler = get_crawler(settings_dict=spidermon_enabled_settings)
     spider = Spider.from_crawler(crawler, "example.com")
     crawler.signals.send_catch_log(
         signal=signals.spider_closed, spider=spider, reason=None
@@ -48,15 +41,10 @@ def test_spider_closed_connect_signal(mocker):
 
 
 @pytest.mark.skipif(sys.version_info < (3, 4), reason="requires python3.4 or higher")
-def test_engine_stopped_connect_signal(mocker):
+def test_engine_stopped_connect_signal(mocker, spidermon_enabled_settings):
     engine_stopped = mocker.patch.object(Spidermon, "engine_stopped")
 
-    crawler = get_crawler(
-        settings_dict={
-            "SPIDERMON_ENABLED": True,
-            "EXTENSIONS": {"spidermon.contrib.scrapy.extensions.Spidermon": 100},
-        }
-    )
+    crawler = get_crawler(settings_dict=spidermon_enabled_settings)
     spider = Spider.from_crawler(crawler, "example.com")
     crawler.signals.send_catch_log(
         signal=signals.engine_stopped, spider=spider, reason=None
