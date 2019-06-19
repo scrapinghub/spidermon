588eafeed23b9d1a5f602e8804293c85a4d5edb5
Renne Rocha <renne@scrapinghub.com>
2019-06-19 08:43:38 -0300

    Added tst to check if template loader path is updated with absolute paths

---
+++ tests/test_templateloader.py
@@ -0,0 +1,18 @@
+import os
+
+from spidermon.templates import TemplateLoader
+
+
+def create_file(path, content):
+    with open(path, "w") as file:
+        file.write(content)
+
+
+def test_get_template_with_absolute_path_updates_templateloader_paths(tmpdir):
+    template_loader = TemplateLoader()
+    test_template = os.path.join(tmpdir, "absolute_path_template.jinja")
+    create_file(test_template, "test content")
+
+    assert tmpdir not in template_loader.paths
+    template_loader.get_template(test_template)
+    assert tmpdir in template_loader.paths
