69078cfab5e1464cfc0660668d5fe28a1e3f09c8
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-06-27 19:30:36 -0300

    Split settings checking from settings inclusion and change checking to be more assertive

--- spidermon/commands/enable.py
+++ spidermon/commands/enable.py
@@ -8,21 +8,21 @@ from os.path import abspath, dirname, join
 import spidermon
 from scrapy.utils.project import inside_project, get_project_settings
 from spidermon.commands.prompts import monitor_prompts
-from spidermon.commands.settings import monitor_settings
-from spidermon.utils.commands import include_settings
+from spidermon.utils.commands import check_settings, include_settings
 
 @click.command('enable', help="Enable Spidermon on your Scrapy project.")
 def enable():
     # check if it's inside a Scrapy project
-    if inside_project():
-        ...
-    else:
+    if not inside_project():
         click.echo('The command must be run inside a Scrapy project.')
         return
 
-    result = include_settings(monitor_settings)
+    # find settings file path
+    module = import_module(get_project_settings().get('BOT_NAME'))
+    settings_path = join(abspath(dirname(module.__file__)), 'settings.py')
 
-    if result:
+    if check_settings(settings_path):
+        include_settings(settings_path)
         click.echo(monitor_prompts['enable_result_true'])
     else:
         click.echo(monitor_prompts['enable_result_false'])
