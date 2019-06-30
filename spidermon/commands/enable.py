f1e8d9719de9fc989adab5a1b310d2b745a66561
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-06-30 16:50:52 -0300

    Improved code readability and efficiency

--- spidermon/commands/enable.py
+++ spidermon/commands/enable.py
@@ -12,16 +12,14 @@ from spidermon.utils.commands import check_settings, include_settings
 
 @click.command('enable', help="Enable Spidermon on your Scrapy project.")
 def enable():
-    # check if it's inside a Scrapy project
     if not inside_project():
-        click.echo('The command must be run inside a Scrapy project.')
+        click.echo(monitor_prompts['project_error'])
         return
 
-    # find settings file path
     module = import_module(get_project_settings().get('BOT_NAME'))
     settings_path = join(abspath(dirname(module.__file__)), 'settings.py')
 
-    if check_settings(settings_path):
+    if is_spidermon_enabled(settings_path):
         include_settings(settings_path)
         click.echo(monitor_prompts['enable_result_true'])
     else:
--- spidermon/commands/setup.py
+++ spidermon/commands/setup.py
@@ -8,31 +8,20 @@ from spidermon.utils.file import copy_template_to_project, render_file
 
 @click.command('setup', help="Setup the monitors from the Scrapy Monitor Suite.")
 def setup():
-    # check if it's inside a Scrapy project
     if not inside_project():
         click.echo(monitor_prompts['project_error'])
         return
 
-    # build string with monitors to be enabled
-    monitors = find_monitors()
     monitors_to_enable = {}
-    for module in monitors:
+    for module in find_monitors():
         for monitor in module['monitors']:
-            result = click.confirm(
-                monitor_prompts['enable'].format(module['monitors'][monitor])
-            )
-            if result:
+            msg = monitor_prompts['enable'].format(module['monitors'][monitor])
+            if click.confirm(msg):
                 monitors_to_enable[monitor] = module['path']
-    monitors_list, monitors_imports = build_monitors_strings(
-        monitors_to_enable
-    )
 
-    # create and build file with monitors
+    monitors_list, imports = build_monitors_strings(monitors_to_enable)
+
     filename = copy_template_to_project('monitor_suite.py.tmpl')
-    render_file(
-        filename,
-        monitors_list=monitors_list,
-        monitors_imports=monitors_imports
-    )
+    render_file(filename, monitors_list=monitors_list, imports=imports)
 
     click.echo(monitor_prompts['response'])
