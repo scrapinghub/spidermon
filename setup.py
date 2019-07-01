d1e19ef0ec3e0e40ec145899aeed877271aa84dc
Renne Rocha <renne@scrapinghub.com>
2019-07-01 08:42:14 -0300

    Bump version: 1.10.1 → 1.10.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requirements = [
 
 setup(
     name="spidermon",
-    version="1.10.1",
+    version="1.10.2",
     url="https://github.com/scrapinghub/spidermon",
     author="Scrapinghub",
     author_email="info@scrapinghub.com",
@@ -22,7 +22,7 @@ setup(
     package_data={"spidermon": ["VERSION"]},
     zip_safe=False,
     include_package_data=True,
-    install_requires=["jsonschema[format]", "python-slugify", "six>=1.10.1"],
+    install_requires=["jsonschema[format]", "python-slugify", "six>=1.10.2"],
     tests_require=test_requirements,
     extras_require={
         # Specific monitors and tools to support notifications and reports
