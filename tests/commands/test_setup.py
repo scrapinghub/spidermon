339d492975af460985e82900c64a714931fab5b9
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-07-13 14:07:50 -0300

    Update with relevant tests

--- tests/commands/test_setup.py
+++ tests/commands/test_setup.py
@@ -1,55 +1,38 @@
 from click.testing import CliRunner
 from spidermon.commands import cli
 from spidermon.commands.prompts import monitor_prompts
+from spidermon.utils.monitors import find_monitor_modules
+
+from unittest.mock import MagicMock
+from pytest_mock import mocker
 
 import pytest
 import spidermon
 
-@pytest.fixture
-def cli(mocker):
-    monitors = [{'Test': 'Foo'}]
-    monitor_string_1 = 'Foo'
-    monitor_string_2 = 'Bar'
-    file = 'path/to/file'
-    mocker.patch('scrapy.utils.project.inside_project', return_value=True)
-    mocker.patch('spidermon.utils.commands.create_file', return_value=file)
-    mocker.patch('spidermon.utils.commands.render_monitors')
-    mocker.patch('spidermon.utils.commands.find_monitors', return_value=monitors)
-    mocker.patch(
-        'spidermon.utils.commands.build_monitors_strings',
-        return_value=(monitor_string_1, monitor_string_2)
-    )
-    return cli
 
-
-def test_should_fail_when_not_in_scrapy_project(cli):
+@pytest.fixture()
+def runner():
     runner = CliRunner()
-    result = runner.invoke(cli, ['setup'])
+    yield runner.isolated_filesystem()
+
+
+def test_should_notify_success_on_spidermon_enabled(runner):
+    result = runner.invoke(cli, ["setup"])
     assert result.exit_code == 0
-    assert 'The command must be run inside a Scrapy project.' in result.output
+    assert result.output == monitor_prompts["enabled"]
 
-def test_should_find_monitors_to_enable():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['setup'])
-    spidermon.utils.commands.find_monitors.assert_called_once()
 
-def test_should_build_monitors_strings():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['setup'])
-    spidermon.utils.commands.build_monitors_strings.assert_called_once_with(
-        monitors
-    )
+def test_should_notify_that_spidermon_was_already_enabled(runner):
+    result = runner.invoke(cli, ["setup"])
+    assert result.exit_code == 0
+    assert result.output == monitor_prompts["already_enabled"]
 
-def test_should_create_suite_file_from_template():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['setup'])
-    spidermon.utils.commands.create_file.assert_called_once_with('monitor_suite.py.tmpl')
 
-def test_should_render_monitor_suite():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['setup'])
-    spidermon.utils.commands.render_monitors.assert_called_once_with(
-        file,
-        monitor_string_1,
-        monitor_string_2
-    )
+def test_should_ask_monitors_to_enable():
+    mocker.patch.object(spidermon.utils.monitors, "find_monitor_modules")
+    result = runner.invoke(cli, ["setup"])
+    assert result.exit_code == 0
+
+
+def test_should_ask_settings_from_monitors_to_enable():
+    ...
---
+++ tests/utils/commands.py
@@ -0,0 +1,43 @@
+from spidermon.utils.commands import (
+    build_monitors_strings,
+    enable_spidermon,
+    get_settings_path,
+    is_setting_setup,
+    is_spidermon_enabled,
+    update_settings,
+)
+
+import pytest
+import spidermon
+
+
+def test_should_return_imports_string():
+    ...
+
+
+def test_should_return_monitors_string():
+    ...
+
+
+def test_should_include_spidermon_on_settings():
+    ...
+
+
+def test_should_update_with_spidermon_extension():
+    ...
+
+
+def test_should_include_spidermon_extension():
+    ...
+
+
+def test_should_return_settings_file_path():
+    ...
+
+
+def test_should_return_spidermon_status():
+    ...
+
+
+def test_should_write_to_settings_file():
+    ...
