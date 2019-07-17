9297ebae62dab4b7791511868ad366cf595703c2
Renne Rocha <renne@scrapinghub.com>
2019-07-17 14:47:03 -0300

    Removed message recipients to avoid calling external API

--- tests/contrib/actions/slack/test_slack_action.py
+++ tests/contrib/actions/slack/test_slack_action.py
@@ -25,10 +25,10 @@ def test_log_text_and_attachment_when_fake_set(logger_info):
     attach_to_be_logged = "attachment content"
 
     manager = SlackMessageManager(
-        sender_token="anything", sender_name="someone", fake=True
+        sender_token="anything", sender_name="@someone", fake=True
     )
     manager.send_message(
-        to=["someone"], text=text_to_be_logged, attachments=attach_to_be_logged
+        to=[], text=text_to_be_logged, attachments=attach_to_be_logged
     )
 
     assert logger_info.call_count == 2
@@ -40,9 +40,9 @@ def test_do_not_log_text_when_fake_is_not_set(logger_info):
     text_not_to_be_logged = "text not to be logged"
 
     manager = SlackMessageManager(
-        sender_token="anything", sender_name="someone", fake=False
+        sender_token="anything", sender_name="@someone", fake=False
     )
-    manager.send_message(to=["someone"], text=text_not_to_be_logged)
+    manager.send_message(to=[], text=text_not_to_be_logged)
 
     assert logger_info.call_count == 0
 
@@ -52,10 +52,10 @@ def test_do_not_log_text_and_attach_when_fake_is_not_set(logger_info):
     attach_not_to_be_logged = "attachment content"
 
     manager = SlackMessageManager(
-        sender_token="anything", sender_name="someone", fake=False
+        sender_token="anything", sender_name="@someone", fake=False
     )
     manager.send_message(
-        to=["someone"], text=text_not_to_be_logged, attachments=attach_not_to_be_logged
+        to=[], text=text_not_to_be_logged, attachments=attach_not_to_be_logged
     )
 
     assert logger_info.call_count == 0
