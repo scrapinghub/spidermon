42995e527035c6722e902e51a9d3270db9cf04df
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-08-05 12:50:41 -0300

    Remove the word monitor from monitor names

--- spidermon/utils/monitors.py
+++ spidermon/utils/monitors.py
@@ -4,28 +4,28 @@ def find_monitor_modules():
             "path": "spidermon.contrib.scrapy.monitors",
             "monitors": {
                 "ItemCountMonitor": {
-                    "name": "Item Count Monitor",
+                    "name": "Item Count",
                     "setting": "SPIDERMON_MIN_ITEMS",
                     "setting_string": "SPIDERMON_MIN_ITEMS = {}",
                     "description": "items",
                     "setting_type": "limit_least",
                 },
                 "ErrorCountMonitor": {
-                    "name": "Error Count Monitor",
+                    "name": "Error Count",
                     "setting": "SPIDERMON_MAX_ERRORS",
                     "setting_string": "SPIDERMON_MAX_ERRORS = {}",
                     "description": "errors",
                     "setting_type": "limit_most",
                 },
                 "FinishReasonMonitor": {
-                    "name": "Finish Reason Monitor",
+                    "name": "Finish Reason",
                     "setting": "SPIDERMON_EXPECTED_FINISH_REASONS",
                     "setting_string": "SPIDERMON_EXPECTED_FINISH_REASONS = {}",
                     "description": "finish reasons",
                     "setting_type": "list",
                 },
                 "UnwantedHTTPCodesMonitor": {
-                    "name": "Unwanted HTTP Code Monitor",
+                    "name": "Unwanted HTTP Codes",
                     "setting": "SPIDERMON_UNWANTED_HTTP_CODES",
                     "setting_string": "SPIDERMON_UNWANTED_HTTP_CODES = {}",
                     "description": "unwanted HTTP codes",
