5b1e0ce3b4d5a9aff42540d988f311f99afc0c8f
Júlio César Batista <julio.batista@outlook.com>
2019-09-16 16:47:17 -0300

    Format files

--- spidermon/utils/settings.py
+++ spidermon/utils/settings.py
@@ -19,23 +19,23 @@ def getdictorlist(crawler, name, default=None):
 
 
 def get_aws_credentials(settings):
-    aws_access_key_id = settings.get('SPIDERMON_AWS_ACCESS_KEY')
-    aws_secret_access_key = settings.get('SPIDERMON_AWS_SECRET_KEY')
+    aws_access_key_id = settings.get("SPIDERMON_AWS_ACCESS_KEY")
+    aws_secret_access_key = settings.get("SPIDERMON_AWS_SECRET_KEY")
 
     if aws_access_key_id and aws_secret_access_key:
         warnings.warn(
-            'SPIDERMON_AWS_ACCESS_KEY and SPIDERMON_AWS_SECRET_KEY are deprecated. '
-            'Please update them to SPIDERMON_AWS_ACCESS_KEY_ID and SPIDERMON_AWS_SECRET_ACCESS_KEY. '  # noqa
-            'Scrapy settings AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are also valid.',
-            DeprecationWarning
+            "SPIDERMON_AWS_ACCESS_KEY and SPIDERMON_AWS_SECRET_KEY are deprecated. "
+            "Please update them to SPIDERMON_AWS_ACCESS_KEY_ID and SPIDERMON_AWS_SECRET_ACCESS_KEY. "  # noqa
+            "Scrapy settings AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are also valid.",
+            DeprecationWarning,
         )
 
-    if not aws_access_key_id and not  aws_secret_access_key:
-        aws_access_key_id = settings.get('SPIDERMON_AWS_ACCESS_KEY_ID')
-        aws_secret_access_key = settings.get('SPIDERMON_AWS_SECRET_ACCESS_KEY')
+    if not aws_access_key_id and not aws_secret_access_key:
+        aws_access_key_id = settings.get("SPIDERMON_AWS_ACCESS_KEY_ID")
+        aws_secret_access_key = settings.get("SPIDERMON_AWS_SECRET_ACCESS_KEY")
 
-    if not aws_access_key_id and not  aws_secret_access_key:
-        aws_access_key_id = settings.get('AWS_ACCESS_KEY_ID')
-        aws_secret_access_key = settings.get('AWS_SECRET_ACCESS_KEY')
+    if not aws_access_key_id and not aws_secret_access_key:
+        aws_access_key_id = settings.get("AWS_ACCESS_KEY_ID")
+        aws_secret_access_key = settings.get("AWS_SECRET_ACCESS_KEY")
 
     return (aws_access_key_id, aws_secret_access_key)
--- tests/utils/test_settings.py
+++ tests/utils/test_settings.py
@@ -13,64 +13,74 @@ def test_spidermon_aws_credentials_not_set():
 
 
 def test_spidermon_aws_credentials():
-    settings = Settings({
-        'SPIDERMON_AWS_ACCESS_KEY': 'aws_access_key',
-        'SPIDERMON_AWS_SECRET_KEY': 'aws_secret_key'
-    })
+    settings = Settings(
+        {
+            "SPIDERMON_AWS_ACCESS_KEY": "aws_access_key",
+            "SPIDERMON_AWS_SECRET_KEY": "aws_secret_key",
+        }
+    )
 
     (aws_access_key_id, aws_secret_access_key) = get_aws_credentials(settings)
 
-    assert aws_access_key_id == 'aws_access_key'
-    assert aws_secret_access_key == 'aws_secret_key'
+    assert aws_access_key_id == "aws_access_key"
+    assert aws_secret_access_key == "aws_secret_key"
 
 
 def test_spidermon_aws_credentials_scrapy_like():
-    settings = Settings({
-        'SPIDERMON_AWS_ACCESS_KEY_ID': 'aws_access_key_id',
-        'SPIDERMON_AWS_SECRET_ACCESS_KEY': 'aws_secret_access_key'
-    })
+    settings = Settings(
+        {
+            "SPIDERMON_AWS_ACCESS_KEY_ID": "aws_access_key_id",
+            "SPIDERMON_AWS_SECRET_ACCESS_KEY": "aws_secret_access_key",
+        }
+    )
 
     (aws_access_key_id, aws_secret_access_key) = get_aws_credentials(settings)
 
-    assert aws_access_key_id == 'aws_access_key_id'
-    assert aws_secret_access_key == 'aws_secret_access_key'
+    assert aws_access_key_id == "aws_access_key_id"
+    assert aws_secret_access_key == "aws_secret_access_key"
 
 
 def test_spidermon_aws_credentials_fall_back_to_scrapy():
-    settings = Settings({
-        'AWS_ACCESS_KEY_ID': 'scrapy_aws_access_key_id',
-        'AWS_SECRET_ACCESS_KEY': 'scrapy_aws_secret_access_key'
-    })
+    settings = Settings(
+        {
+            "AWS_ACCESS_KEY_ID": "scrapy_aws_access_key_id",
+            "AWS_SECRET_ACCESS_KEY": "scrapy_aws_secret_access_key",
+        }
+    )
 
     (aws_access_key_id, aws_secret_access_key) = get_aws_credentials(settings)
 
-    assert aws_access_key_id == 'scrapy_aws_access_key_id'
-    assert aws_secret_access_key == 'scrapy_aws_secret_access_key'
+    assert aws_access_key_id == "scrapy_aws_access_key_id"
+    assert aws_secret_access_key == "scrapy_aws_secret_access_key"
 
 
 def test_spidermon_aws_credentials_are_preferred_over_scrapy_ones():
-    settings = Settings({
-        'AWS_ACCESS_KEY_ID': 'scrapy_aws_access_key_id',
-        'AWS_SECRET_ACCESS_KEY': 'scrapy_aws_secret_access_key',
-        'SPIDERMON_AWS_ACCESS_KEY_ID': 'spidermon_aws_access_key_id',
-        'SPIDERMON_AWS_SECRET_ACCESS_KEY': 'spidermon_aws_secret_access_key'
-    })
+    settings = Settings(
+        {
+            "AWS_ACCESS_KEY_ID": "scrapy_aws_access_key_id",
+            "AWS_SECRET_ACCESS_KEY": "scrapy_aws_secret_access_key",
+            "SPIDERMON_AWS_ACCESS_KEY_ID": "spidermon_aws_access_key_id",
+            "SPIDERMON_AWS_SECRET_ACCESS_KEY": "spidermon_aws_secret_access_key",
+        }
+    )
 
     (aws_access_key_id, aws_secret_access_key) = get_aws_credentials(settings)
 
-    assert aws_access_key_id == 'spidermon_aws_access_key_id'
-    assert aws_secret_access_key == 'spidermon_aws_secret_access_key'
+    assert aws_access_key_id == "spidermon_aws_access_key_id"
+    assert aws_secret_access_key == "spidermon_aws_secret_access_key"
 
 
 def test_spidermon_old_aws_credentials_are_preferred_over_new_ones():
-    settings = Settings({
-        'SPIDERMON_AWS_ACCESS_KEY': 'old_aws_access_key',
-        'SPIDERMON_AWS_SECRET_KEY': 'old_aws_secret_key',
-        'SPIDERMON_AWS_ACCESS_KEY_ID': 'new_aws_access_key_id',
-        'SPIDERMON_AWS_SECRET_ACCESS_KEY': 'new_aws_secret_access_key'
-    })
+    settings = Settings(
+        {
+            "SPIDERMON_AWS_ACCESS_KEY": "old_aws_access_key",
+            "SPIDERMON_AWS_SECRET_KEY": "old_aws_secret_key",
+            "SPIDERMON_AWS_ACCESS_KEY_ID": "new_aws_access_key_id",
+            "SPIDERMON_AWS_SECRET_ACCESS_KEY": "new_aws_secret_access_key",
+        }
+    )
 
     (aws_access_key_id, aws_secret_access_key) = get_aws_credentials(settings)
 
-    assert aws_access_key_id == 'old_aws_access_key'
-    assert aws_secret_access_key == 'old_aws_secret_key'
+    assert aws_access_key_id == "old_aws_access_key"
+    assert aws_secret_access_key == "old_aws_secret_key"
