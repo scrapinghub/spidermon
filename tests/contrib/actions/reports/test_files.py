9d4941c40f6aa68d5c0326106d1bada08efadf52
Felipe Fin <felipe.lunkes@gmail.com>
2019-12-04 13:17:06 -0300

    Removed TestCase class and used pytest-mock to avoid imports for different python versions

--- tests/contrib/actions/reports/test_files.py
+++ tests/contrib/actions/reports/test_files.py
@@ -1,9 +1,4 @@
-try:
-    from unittest.mock import MagicMock, patch, mock_open
-except ImportError:
-    from mock import MagicMock, patch, mock_open
 import pytest
-from unittest import TestCase
 
 from spidermon.contrib.actions.reports.files import CreateFileReport
 from spidermon.exceptions import NotConfigured
@@ -12,32 +7,35 @@ TEMPLATE = "template"
 FILENAME = "filename"
 
 
-class CreateFileReportTests(TestCase):
-    def test_init_without_params(self):
-        with pytest.raises(TypeError):
-            CreateFileReport()
+def test_init_without_params():
+    with pytest.raises(TypeError):
+        CreateFileReport()
 
-    def test_init_with_template_param_only(self):
-        with pytest.raises(TypeError):
-            CreateFileReport(template=TEMPLATE)
 
-    def test_init_with_invalid_params(self):
-        with pytest.raises(NotConfigured) as e:
-            CreateFileReport(template=TEMPLATE, filename=None)
-        assert "You must define a template output file." in str(e.value)
+def test_init_with_template_param_only():
+    with pytest.raises(TypeError):
+        CreateFileReport(template=TEMPLATE)
 
-    def test_init_with_valid_params(self):
-        report = CreateFileReport(template=TEMPLATE, filename=FILENAME)
-        assert report.filename == FILENAME
 
-    @patch("spidermon.contrib.actions.reports.files.open", new_callable=mock_open())
-    def test_after_render_report(self, _open):
-        report = CreateFileReport(template=TEMPLATE, filename=FILENAME)
-        report.render_template = MagicMock(return_value="render_template")
-        report.render_text_template = MagicMock(return_value="rendered_filename")
-        report.report = "report data"
-        report.after_render_report()
-        report.render_text_template.assert_called_once_with(FILENAME)
-        _open.assert_called_once_with("rendered_filename", "w")
-        # Write is inside a context manager, so we need to call __enter__()
-        _open().__enter__().write.assert_called_once_with("report data")
+def test_init_with_invalid_params():
+    with pytest.raises(NotConfigured) as e:
+        CreateFileReport(template=TEMPLATE, filename=None)
+    assert "You must define a template output file." in str(e.value)
+
+
+def test_init_with_valid_params():
+    report = CreateFileReport(template=TEMPLATE, filename=FILENAME)
+    assert report.filename == FILENAME
+
+
+def test_after_render_report(mocker):
+    _open = mocker.patch("spidermon.contrib.actions.reports.files.open", new_callable=mocker.mock_open())
+    report = CreateFileReport(template=TEMPLATE, filename=FILENAME)
+    report.render_template = mocker.MagicMock(return_value="render_template")
+    report.render_text_template = mocker.MagicMock(return_value="rendered_filename")
+    report.report = "report data"
+    report.after_render_report()
+    report.render_text_template.assert_called_once_with(FILENAME)
+    _open.assert_called_once_with("rendered_filename", "w")
+    # Write is inside a context manager, so we need to call __enter__()
+    _open().__enter__().write.assert_called_once_with("report data")
