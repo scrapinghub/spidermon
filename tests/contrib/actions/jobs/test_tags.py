28a3dea8dfed5d3f6eb76e58ef9f7e0a0e319c94
Felipe Fin <felipe.lunkes@gmail.com>
2019-12-05 15:52:15 -0300

    Removed unnecessary imports

--- tests/contrib/actions/jobs/test_tags.py
+++ tests/contrib/actions/jobs/test_tags.py
@@ -1,9 +1,4 @@
-try:
-    from unittest.mock import MagicMock
-except ImportError:
-    from mock import MagicMock
 import pytest
-from unittest import TestCase
 
 from spidermon.contrib.actions.jobs.tags import JobTagsAction, AddJobTags, RemoveJobTags
 from spidermon.exceptions import NotConfigured
@@ -19,26 +14,26 @@ def test_job_tags_action_init(tags, expected_tags):
     assert job_tags.tags == expected_tags
 
 
-class JobTagsActionTests(TestCase):
+class JobTagsActionTests:
     def test_run_action_without_data(self):
         job_tags = JobTagsAction(TAG)
         with pytest.raises(AttributeError):
             job_tags.run_action()
 
-    def test_run_action_without_data_job(self):
+    def test_run_action_without_data_job(self, mocker):
         job_tags = JobTagsAction(TAG)
-        job_tags.data = MagicMock()
+        job_tags.data = mocker.MagicMock()
         job_tags.data.job = ""
         with pytest.raises(NotConfigured) as e:
             job_tags.run_action()
         assert "Job not available!" in str(e.value)
 
-    def test_run_action(self):
+    def test_run_action(self, mocker):
         job_tags = JobTagsAction(TAG)
-        job_tags.data = MagicMock()
-        metadata = MagicMock()
+        job_tags.data = mocker.MagicMock()
+        metadata = mocker.MagicMock()
         job_tags.data.job.metadata = metadata
-        job_tags.process_tags = MagicMock()
+        job_tags.process_tags = mocker.MagicMock()
         job_tags.run_action()
         job_tags.process_tags.assert_called_once_with(metadata)
         job_tags.data.job.metadata.save.assert_called_once_with()
@@ -49,7 +44,7 @@ def test_add_job_tags_properties():
     assert job_tags.tag_settings == "SPIDERMON_JOB_TAGS_TO_ADD"
 
 
-class AddJobTagsTests(TestCase):
+class AddJobTagsTests:
     def test_process_tags(self):
         metadata = {"tags": []}
         job_tags = AddJobTags(TAG)
@@ -62,7 +57,7 @@ def test_remove_job_tags_properties():
     assert job_tags.tag_settings == "SPIDERMON_JOB_TAGS_TO_REMOVE"
 
 
-class RemoveJobTagsTests(TestCase):
+class RemoveJobTagsTests:
     def test_process_tags(self):
         metadata = {"tags": [TAG]}
         job_tags = RemoveJobTags(TAG)
