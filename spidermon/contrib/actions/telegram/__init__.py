9296cda7c257b50c38f9b3960ac3cd7f3260117f
Ana Paula Gomes <apgomes88@gmail.com>
2020-03-04 16:55:49 +0100

    Log the whole response

--- spidermon/contrib/actions/telegram/__init__.py
+++ spidermon/contrib/actions/telegram/__init__.py
@@ -26,7 +26,7 @@ class SimplyTelegramClient:
         r = requests.get(api_url).json()
         if r.get("ok") is False:
             logger.error(
-                "Failed to send message. Telegram api error: %s", r.description
+                "Failed to send message. Telegram api error: %s", json.dumps(r)
             )
 
 
