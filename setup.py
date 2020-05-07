bcbd66ed918b688d079e72f587ad3f4ce60581a4
Renne Rocha <renne@rennerocha.com>
2020-05-07 09:00:25 -0300

    Bump version: 1.12.0 → 1.12.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name="spidermon",
-    version="1.12.0",
+    version="1.12.1",
     url="https://github.com/scrapinghub/spidermon",
     author="Scrapinghub",
     author_email="info@scrapinghub.com",
@@ -25,7 +25,7 @@ setup(
     package_data={"spidermon": ["VERSION"]},
     zip_safe=False,
     include_package_data=True,
-    install_requires=["jsonschema[format]", "python-slugify", "six>=1.12.0"],
+    install_requires=["jsonschema[format]", "python-slugify", "six>=1.12.1"],
     tests_require=test_requirements,
     extras_require={
         # Specific monitors and tools to support notifications and reports
--- spidermon/__init__.py
+++ spidermon/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = "1.12.0"
+__version__ = "1.12.1"
 
 from .core.monitors import Monitor
 from .core.suites import MonitorSuite
