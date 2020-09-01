f52d5f4416ee8ea6ee92ba4187aad030be8791d6
Anderson Berg <andersonberg@gmail.com>
2020-09-01 16:43:10 -0300

    removing tests for api_call function

--- tests/contrib/actions/slack/test_slack_message_manager.py
+++ tests/contrib/actions/slack/test_slack_message_manager.py
@@ -11,42 +11,6 @@ def logger_error(mocker):
     return mocker.patch("spidermon.contrib.actions.slack.logger.error")
 
 
-@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
-def test_api_call_with_no_error_should_not_log_messages(mocker, logger_error):
-    api_valid_message = {"ok": True, "text": "valid response"}
-    mocker.patch(
-        "spidermon.contrib.actions.slack.WebClient.api_call",
-        return_value=api_valid_message,
-    )
-    manager = SlackMessageManager("SENDER_TOKEN", "SENDER_NAME")
-
-    manager._api_call("some_method")
-
-    assert logger_error.call_count == 0
-
-
-@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
-def test_api_call_with_error_should_log_error_msg(mocker, logger_error):
-    error_msg = "API call failure"
-    # API call response with error (see https://api.slack.com/rtm#errors)
-    api_error_message = {
-        "ok": False,
-        "reply_to": 1,
-        "error": {"code": 2, "msg": error_msg},
-    }
-    mocker.patch(
-        "spidermon.contrib.actions.slack.WebClient.api_call",
-        return_value=api_error_message,
-    )
-
-    manager = SlackMessageManager("SENDER_TOKEN", "SENDER_NAME")
-
-    manager._api_call("some_method")
-
-    assert logger_error.call_count == 1
-    assert error_msg in logger_error.call_args_list[0][0]
-
-
 @pytest.mark.parametrize("recipients", ["foo,bar", ["foo", "bar"]])
 def test_load_recipients_list_from_crawler_settings(recipients):
     settings = {"SPIDERMON_SLACK_RECIPIENTS": recipients}
