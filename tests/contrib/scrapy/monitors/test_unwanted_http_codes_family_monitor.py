15f7ba4d11d30301c229fd1eb3dfe8be1d3436bc
Muhammad Shafiq <muhammad.shafiq@zyte.com>
2021-09-23 16:47:33 +0500

    added test case

--- tests/contrib/scrapy/monitors/test_unwanted_http_codes_family_monitor.py
+++ tests/contrib/scrapy/monitors/test_unwanted_http_codes_family_monitor.py
@@ -24,3 +24,21 @@ def test_unwanted_client_errors_monitor_should_fail_zero(make_data):
     data["stats"]["downloader/response_status_count/503"] = 21
     runner.run(suite, **data)
     assert "Found 6 client errors in log" in runner.result.monitor_results[0].error
+
+
+def test_unwanted_client_errors_monitor_should_pass(make_data):
+    """UnwantedHTTPCodesFamilyMonitor should pass if the # of sum of 4xx response codes does not exceed the limit.
+    The limit is 4.
+    UnwantedHTTPCodesFamilyMonitor should pass if the # of sum of 5xx response codes does not exceed the limit.
+    The limit is 12.
+    """
+
+    data = make_data({SPIDERMON_MAX_CLIENT_HTTP_ERRORS: 5, SPIDERMON_MAX_SERVER_HTTP_ERRORS: 12})
+    runner = data.pop("runner")
+    suite = new_suite()
+    data["stats"]["downloader/response_status_count/400"] = 1
+    data["stats"]["downloader/response_status_count/404"] = 3
+    data["stats"]["downloader/response_status_count/503"] = 4
+    data["stats"]["downloader/response_status_count/504"] = 5
+    runner.run(suite, **data)
+    assert runner.result.monitor_results[0].error is None
