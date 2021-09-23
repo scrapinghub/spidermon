57c6be27a38cfee966779733b75e76114006add8
further-reading <roy.healy87+gh@gmail.com>
2021-09-23 14:06:36 +0100

    [SlackMessageManager] Adding tests https://github.com/scrapinghub/spidermon/issues/300

--- tests/contrib/actions/slack/test_slack_message_manager.py
+++ tests/contrib/actions/slack/test_slack_message_manager.py
@@ -1,4 +1,3 @@
-import sys
 import pytest
 
 from scrapy.utils.test import get_crawler
@@ -11,9 +10,32 @@ def logger_error(mocker):
     return mocker.patch("spidermon.contrib.actions.slack.logger.error")
 
 
+@pytest.fixture
+def mock_webclient(mocker):
+    return mocker.patch("spidermon.contrib.actions.slack.WebClient")
+
+
 @pytest.mark.parametrize("recipients", ["foo,bar", ["foo", "bar"]])
 def test_load_recipients_list_from_crawler_settings(recipients):
     settings = {"SPIDERMON_SLACK_RECIPIENTS": recipients}
     crawler = get_crawler(settings_dict=settings)
     kwargs = SendSlackMessage.from_crawler_kwargs(crawler)
     assert kwargs["recipients"] == ["foo", "bar"]
+
+
+def test_get_valid_icon_url(mock_webclient):
+    mock_webclient().users_list.return_value = {
+        "members": [{"name": "test_valid_user", "profile": {"image_48": "fake.jpg"}}]
+    }
+    manager = SlackMessageManager(sender_token="Fake", sender_name="test_valid_user")
+    url = manager._get_icon_url()
+    assert url == "fake.jpg"
+
+
+def test_get_invalid_user_icon_url(mock_webclient):
+    mock_webclient().users_list.return_value = {
+        "members": [{"name": "test_valid_user", "profile": {"image_48": "fake.jpg"}}]
+    }
+    manager = SlackMessageManager(sender_token="Fake", sender_name="test_invalid_user")
+    url = manager._get_icon_url()
+    assert url is None
