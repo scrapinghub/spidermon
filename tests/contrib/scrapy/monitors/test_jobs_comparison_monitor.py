84b1f66f3b40bfc557e282adc0a457fcd10ca762
Thiago Curvelo <tcurvelo@gmail.com>
2021-11-29 12:44:21 -0300

    JobsComparionMonitor: update tests mocking the hubstorage client

--- tests/contrib/scrapy/monitors/test_jobs_comparison_monitor.py
+++ tests/contrib/scrapy/monitors/test_jobs_comparison_monitor.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 
 import pytest
 from spidermon import MonitorSuite
@@ -11,27 +11,55 @@ from spidermon.contrib.scrapy.monitors import (
 
 
 @pytest.fixture
-def client(previous_counts):
-    _client = MagicMock()
-    _client.jobs = []
-    for count in previous_counts:
-        job = MagicMock()
-        job.stats.item_scraped_count = count
-        _client.jobs.append(job)
-    return _client
+def mock_client(previous_counts):
+    return Mock(
+        **{'spider.jobs.list.return_value': [dict(items=c) for c in previous_counts]}
+    )
 
 
-def new_suite():
+@pytest.fixture
+def mock_suite(mock_client, monkeypatch):
+    monkeypatch.setattr(JobsComparisonMonitor, "client", mock_client)
     return MonitorSuite(monitors=[JobsComparisonMonitor])
 
 
 @pytest.mark.parametrize(
-    ["previous_jobs", "expected"], [(0, True), (1, True), (5, True)]
+    ["number_of_jobs", "item_count", "previous_counts", "expected"],
+    [
+        (0, 1, [1], False),
+        (1, 0, [1], True),
+        (5, 0, [1], True),
+    ],
 )
-def test_jobs_comparison_monitor_is_enabled(make_data, previous_jobs, expected):
-    data = make_data({SPIDERMON_JOBS_COMPARISON: previous_jobs})
+def test_jobs_comparison_monitor_is_enabled(
+    make_data, mock_suite, item_count, number_of_jobs, expected
+):
+    data = make_data({SPIDERMON_JOBS_COMPARISON: number_of_jobs})
+    data["stats"]["item_scraped_count"] = item_count
     runner = data.pop("runner")
-    runner.run(new_suite(), **data)
+    runner.run(mock_suite, **data)
 
     is_enabled = runner.result.monitor_results[0].error is not None
     assert is_enabled == expected
+
+
+@pytest.mark.parametrize(
+    ["item_count", "previous_counts", "threshold", "should_raise"],
+    [
+        (90, [100], 0.9, False),
+        (90, [100, 101], 0.9, True),
+        (80, [100, 101, 99], 0.8, False),
+        (80, [100, 101, 99, 102], 0.8, True),
+    ],
+)
+def test_jobs_comparison_monitor(
+    make_data, mock_suite, item_count, threshold, should_raise
+):
+    data = make_data(
+        {SPIDERMON_JOBS_COMPARISON: 1, SPIDERMON_JOBS_COMPARISON_THRESHOLD: threshold}
+    )
+    data["stats"]["item_scraped_count"] = item_count
+    runner = data.pop("runner")
+    runner.run(mock_suite, **data)
+
+    assert should_raise == bool(runner.result.monitor_results[0].error)
