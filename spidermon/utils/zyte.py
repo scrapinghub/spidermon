1e1622eb32bb6ad64ad39012384ac3beada74fc4
Thiago Curvelo <tcurvelo@gmail.com>
2022-01-12 20:26:06 -0300

    utils/zyte: rename `hubstorage` module to `zyte` and update it
    - use `ScrapinhubClient` instead of the deprecated `HubstorageClient`
    - add spider() method

---
+++ spidermon/utils/zyte.py
@@ -0,0 +1,59 @@
+"""
+Module to hold a singleton to Zyte's Scrapy Cloud client.
+
+It expects that "SHUB_JOBKEY" be present as environment variable, as
+well as either "SH_APIKEY" or "SHUB_JOBAUTH" to access Scrapy Cloud API.
+"""
+import os
+
+from scrapinghub import ScrapinghubClient
+
+
+class Client:
+    def __init__(self):
+        self.available = "SHUB_JOBKEY" in os.environ
+        self._client = None
+        self._project = None
+        self._spider = None
+        self._job = None
+        if self.available:
+            self.job_key = os.environ["SHUB_JOBKEY"]
+            self.project_id, self.spider_id, self.job_id = map(
+                int, self.job_key.split("/")
+            )
+        else:
+            self.project_id = None
+            self.spider_id = None
+            self.job_id = None
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = ScrapinghubClient()
+        return self._client
+
+    @property
+    def project(self):
+        if not self._project:
+            self._project = self.client.get_project(str(self.project_id))
+        return self._project
+
+    @property
+    def spider(self):
+        if not self._spider:
+            spider_name = self.job.metadata.get("spider")
+            self._spider = self.project.spiders.get(spider_name)
+        return self._spider
+
+    @property
+    def job(self):
+        if not self._job:
+            self._job = self.client.get_job(self.job_key)
+        return self._job
+
+    def close(self):
+        if self._client:
+            self._client.close()
+
+
+client = Client()
