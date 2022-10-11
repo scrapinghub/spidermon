9896750701d2bd3cb33f1766dd3e3ff88acaa370
Adilson Neto <adilson.dealmeida@zyte.com>
2022-10-11 11:15:14 -0300

    fix: make changes compatible with python3.6

--- spidermon/contrib/validation/jsonschema/translator.py
+++ spidermon/contrib/validation/jsonschema/translator.py
@@ -35,6 +35,7 @@ class JSONSchemaMessageTranslator(MessageTranslator):
         r"^.* is less than or equal to the minimum of .*$": messages.NUMBER_TOO_LOW,
         r"^.* is not a multiple of .*$": messages.NOT_MULTIPLE_OF,
         r"^.* should not be valid under .*$": messages.NOT_ALLOWED_VALUE,
+        r"^.* is not allowed for .*$": messages.NOT_ALLOWED_VALUE,
         r"^.+ is too short$": messages.FIELD_TOO_SHORT,
         r"^.+ is too long$": messages.FIELD_TOO_LONG,
         r"^.+ does not match .*$": messages.REGEX_NOT_MATCHED,
