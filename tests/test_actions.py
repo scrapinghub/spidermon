a6706c32c5f2a4208733bc8ec1b0e644b9319b78
further-reading <roy.healy87+gh@gmail.com>
2022-11-03 11:43:26 +0000

    Adding more action tests https://github.com/scrapinghub/spidermon/pull/355

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -3,6 +3,46 @@ from spidermon.exceptions import SkipAction
 from unittest.mock import MagicMock
 
 
+def test_action_success():
+    class TestAction(Action):
+        def run_action(self):
+            pass
+
+    result_mock = MagicMock()
+    action = TestAction()
+
+    action.run(result_mock, MagicMock())
+
+    result_mock.add_action_success.assert_called()
+
+
+def test_action_fail():
+    class TestAction(Action):
+        def run_action(self):
+            raise Exception
+
+    result_mock = MagicMock()
+    action = TestAction()
+
+    action.run(result_mock, MagicMock())
+
+    result_mock.add_action_error.assert_called()
+    result_mock.add_action_success.assert_not_called()
+
+
+def test_action_skip():
+    class TestAction(Action):
+        def run_action(self):
+            raise SkipAction("Test")
+
+    result_mock = MagicMock()
+    action = TestAction()
+
+    action.run(result_mock, MagicMock())
+
+    result_mock.add_action_skip.assert_called()
+
+
 def test_fallback_action():
     class TestAction(Action):
         fallback = MagicMock()
@@ -26,6 +66,6 @@ def test_fallback_skip_action():
             raise SkipAction("Test")
 
     action = TestAction()
-    action.run(MagicMock(), MagicMock())
 
+    action.run(MagicMock(), MagicMock())
     action.fallback.assert_not_called()
