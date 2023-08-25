45056881db5239a7b1422c7594ad27e6d522b54b
felipe <felipetdecarli@gmail.com>
2023-08-25 19:13:14 -0300

    fix: format

--- spidermon/contrib/actions/sns/notifiers.py
+++ spidermon/contrib/actions/sns/notifiers.py
@@ -1,45 +1,33 @@
 from . import BaseSNSNotification
 
+
 class SendSNSNotificationSpiderStarted(BaseSNSNotification):
     def run_action(self):
-        print(self)
         subject = "Spider Started"
         attributes = {
-            "EventType": {
-                "DataType": "String",
-                "StringValue": "SpiderStarted"
-            },
-            "SpiderName": {
-                "DataType": "String",
-                "StringValue": self.data.spider.name
-            },
+            "EventType": {"DataType": "String", "StringValue": "SpiderStarted"},
+            "SpiderName": {"DataType": "String", "StringValue": self.data.spider.name},
             "StartTime": {
                 "DataType": "String",
-                "StringValue": str(self.data.stats.start_time)
-            }
+                "StringValue": str(self.data.stats.start_time),
+            },
         }
         self.send_message(subject, attributes)
 
+
 class SendSNSNotificationSpiderFinished(BaseSNSNotification):
     def run_action(self):
-        print(self.data)
         subject = "Spider Finished"
         attributes = {
-            "EventType": {
-                "DataType": "String",
-                "StringValue": "SpiderFinished"
-            },
-            "SpiderName": {
-                "DataType": "String",
-                "StringValue": self.data.spider.name
-            },
+            "EventType": {"DataType": "String", "StringValue": "SpiderFinished"},
+            "SpiderName": {"DataType": "String", "StringValue": self.data.spider.name},
             "ItemsScraped": {
                 "DataType": "Number",
-                "StringValue": str(self.data.stats.item_scraped_count)
+                "StringValue": str(self.data.stats.item_scraped_count),
             },
             "FinishTime": {
                 "DataType": "String",
-                "StringValue": str(self.data.stats.finish_time)
-            }
+                "StringValue": str(self.data.stats.finish_time),
+            },
         }
         self.send_message(subject, attributes)
