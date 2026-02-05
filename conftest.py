5cfb93c6f40bacb974a8aec15fb68ab7b906e1b7
Nilton Junior <ngm.junior@outlook.com>
2026-02-05 10:18:06 -0300

    fix: fallback to prevent get_reactor_settings() from failling
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

--- conftest.py
+++ conftest.py
@@ -3,6 +3,14 @@ from scrapy.crawler import Crawler
 from scrapy.statscollectors import MemoryStatsCollector
 from scrapy import Spider
 
+# Install Twisted reactor before importing scrapy.test utilities
+# This is required for Scrapy 2.13+ with Python 3.13+
+try:
+    from twisted.internet import default
+
+    default.install()
+except (ImportError, RuntimeError):
+    pass  # Reactor may already be installed
 
 pytest_plugins = "spidermon.contrib.pytest.plugins.filter_monitors"
 
