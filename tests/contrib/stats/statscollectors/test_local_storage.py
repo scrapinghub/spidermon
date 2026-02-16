b05b09a472673a338234c19e7a1f707d58d22700
Adrian Chaves <adrian@zyte.com>
2026-02-16 14:53:58 +0100

    In tests, use crawler.stop_async() if available

--- tests/contrib/stats/statscollectors/test_local_storage.py
+++ tests/contrib/stats/statscollectors/test_local_storage.py
@@ -8,9 +8,17 @@ pytest.importorskip("scrapy")
 from scrapy import Spider
 from scrapy.utils.test import get_crawler
 from scrapy.utils.project import data_path
+from scrapy.utils.defer import deferred_f_from_coro_f
 from spidermon.contrib.stats.statscollectors.local_storage import (
     LocalStorageStatsHistoryCollector,
 )
+
+
+async def stop_crawler(crawler):
+    if hasattr(crawler, "stop_async"):
+        await crawler.stop_async()
+    else:
+        await crawler.stop()
 
 
 @pytest.fixture
@@ -31,7 +39,8 @@ def test_settings():
     }
 
 
-def test_spider_has_stats_history_attribute_when_opened_with_collector(
+@deferred_f_from_coro_f
+async def test_spider_has_stats_history_attribute_when_opened_with_collector(
     test_settings, stats_temporary_location
 ):
     crawler = get_crawler(Spider, test_settings)
@@ -39,10 +48,11 @@ def test_spider_has_stats_history_attribute_when_opened_with_collector(
     crawler.stats.set_value("garbage", "value")
     assert hasattr(crawler.spider, "stats_history")
     assert crawler.spider.stats_history == deque()
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_has_stats_history_queue_with_specified_max_size(
+@deferred_f_from_coro_f
+async def test_spider_has_stats_history_queue_with_specified_max_size(
     test_settings, stats_temporary_location
 ):
     max_stored_stats = 2
@@ -52,85 +62,89 @@ def test_spider_has_stats_history_queue_with_specified_max_size(
     crawler.crawl("foo_spider")
     assert crawler.spider.stats_history == deque()
     assert crawler.spider.stats_history.maxlen == max_stored_stats
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
 @pytest.mark.parametrize("initial_max_len,end_max_len", [(5, 2), (5, 10), (5, 5)])
-def test_spider_update_stats_history_queue_max_size(
+@deferred_f_from_coro_f
+async def test_spider_update_stats_history_queue_max_size(
     test_settings, stats_temporary_location, initial_max_len, end_max_len
 ):
     test_settings["SPIDERMON_MAX_STORED_STATS"] = initial_max_len
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert crawler.spider.stats_history.maxlen == initial_max_len
-    crawler.stop()
+    await stop_crawler(crawler)
 
     test_settings["SPIDERMON_MAX_STORED_STATS"] = end_max_len
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert crawler.spider.stats_history.maxlen == end_max_len
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_has_last_stats_history_when_opened_again(
+@deferred_f_from_coro_f
+async def test_spider_has_last_stats_history_when_opened_again(
     test_settings, stats_temporary_location
 ):
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("first_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert len(crawler.spider.stats_history) == 1
     assert crawler.spider.stats_history[0]["first_execution"] == "value"
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_has_two_last_stats_history_when_opened_third_time(
+@deferred_f_from_coro_f
+async def test_spider_has_two_last_stats_history_when_opened_third_time(
     test_settings, stats_temporary_location
 ):
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("first_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("second_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert len(crawler.spider.stats_history) == 2
     assert "second_execution" in crawler.spider.stats_history[0].keys()
     assert "first_execution" in crawler.spider.stats_history[1].keys()
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_limit_number_of_stored_stats(test_settings, stats_temporary_location):
+@deferred_f_from_coro_f
+async def test_spider_limit_number_of_stored_stats(test_settings, stats_temporary_location):
     test_settings["SPIDERMON_MAX_STORED_STATS"] = 2
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("first_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("second_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("third_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert len(crawler.spider.stats_history) == 2
     assert "third_execution" in crawler.spider.stats_history[0].keys()
     assert "second_execution" in crawler.spider.stats_history[1].keys()
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
 def test_able_to_import_deprecated_local_storage_stats_collector_module():
@@ -155,7 +169,8 @@ def test_able_to_import_deprecated_local_storage_stats_collector_module():
         ), f"Unable to import spidermon.contrib.stats.statscollectors.LocalStorageStatsHistoryCollector"
 
 
-def test_stats_location_env_spider_name(test_settings):
+@deferred_f_from_coro_f
+async def test_stats_location_env_spider_name(test_settings):
     statsdir = data_path("stats", createdir=False)
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
@@ -165,11 +180,12 @@ def test_stats_location_env_spider_name(test_settings):
     actual = crawler.stats._stats_location(crawler.spider)
     expected = os.path.join(statsdir, "virtual_spider_stats_history")
     assert actual == expected
-    crawler.stop()
+    await stop_crawler(crawler)
     del os.environ["SHUB_VIRTUAL_SPIDER"]
 
 
-def test_stats_location_regular_spider_name(test_settings):
+@deferred_f_from_coro_f
+async def test_stats_location_regular_spider_name(test_settings):
     statsdir = data_path("stats", createdir=False)
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
@@ -177,4 +193,4 @@ def test_stats_location_regular_spider_name(test_settings):
     actual = crawler.stats._stats_location(crawler.spider)
     expected = os.path.join(statsdir, "foo_spider_stats_history")
     assert actual == expected
-    crawler.stop()
+    await stop_crawler(crawler)
--- tests/contrib/stats/statscollectors/test_sc_collections.py
+++ tests/contrib/stats/statscollectors/test_sc_collections.py
@@ -9,12 +9,20 @@ import scrapinghub
 
 from packaging.version import Version
 from scrapy import Spider
+from scrapy.utils.defer import deferred_f_from_coro_f
 from scrapy.utils.test import get_crawler
 from spidermon.contrib.stats.statscollectors.sc_collections import (
     ScrapyCloudCollectionsStatsHistoryCollector,
 )
 
 from tests import SCRAPY_VERSION
+
+
+async def stop_crawler(crawler):
+    if hasattr(crawler, "stop_async"):
+        await crawler.stop_async()
+    else:
+        await crawler.stop()
 
 
 class StoreMock:
@@ -87,7 +95,8 @@ def test_open_collection_with_api(scrapinghub_mock, os_environ_mock, test_settin
     assert store is not None
 
 
-def test_spider_has_stats_history_attribute_when_opened_with_collector(
+@deferred_f_from_coro_f
+async def test_spider_has_stats_history_attribute_when_opened_with_collector(
     test_settings, stats_collection
 ):
     crawler = get_crawler(Spider, test_settings)
@@ -95,10 +104,11 @@ def test_spider_has_stats_history_attribute_when_opened_with_collector(
     crawler.stats.set_value("garbage", "value")
     assert hasattr(crawler.spider, "stats_history")
     assert crawler.spider.stats_history == deque()
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_has_stats_history_queue_with_specified_max_size(
+@deferred_f_from_coro_f
+async def test_spider_has_stats_history_queue_with_specified_max_size(
     test_settings, stats_collection
 ):
     max_stored_stats = 2
@@ -108,93 +118,98 @@ def test_spider_has_stats_history_queue_with_specified_max_size(
     crawler.crawl("foo_spider")
     assert crawler.spider.stats_history == deque()
     assert crawler.spider.stats_history.maxlen == max_stored_stats
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
 @pytest.mark.parametrize("initial_max_len,end_max_len", [(5, 2), (5, 10), (5, 5)])
-def test_spider_update_stats_history_queue_max_size(
+@deferred_f_from_coro_f
+async def test_spider_update_stats_history_queue_max_size(
     test_settings, stats_collection, initial_max_len, end_max_len
 ):
     test_settings["SPIDERMON_MAX_STORED_STATS"] = initial_max_len
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert crawler.spider.stats_history.maxlen == initial_max_len
-    crawler.stop()
+    await stop_crawler(crawler)
 
     test_settings["SPIDERMON_MAX_STORED_STATS"] = end_max_len
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert crawler.spider.stats_history.maxlen == end_max_len
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_has_last_stats_history_when_opened_again(
+@deferred_f_from_coro_f
+async def test_spider_has_last_stats_history_when_opened_again(
     test_settings, stats_collection
 ):
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("first_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert len(crawler.spider.stats_history) == 1
     assert crawler.spider.stats_history[0]["first_execution"] == "value"
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_has_two_last_stats_history_when_opened_third_time(
+@deferred_f_from_coro_f
+async def test_spider_has_two_last_stats_history_when_opened_third_time(
     test_settings, stats_collection
 ):
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("first_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("second_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert len(crawler.spider.stats_history) == 2
     assert "second_execution" in crawler.spider.stats_history[0].keys()
     assert "first_execution" in crawler.spider.stats_history[1].keys()
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
-def test_spider_limit_number_of_stored_stats(test_settings, stats_collection):
+@deferred_f_from_coro_f
+async def test_spider_limit_number_of_stored_stats(test_settings, stats_collection):
     test_settings["SPIDERMON_MAX_STORED_STATS"] = 2
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("first_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("second_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     crawler.stats.set_value("third_execution", "value")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
     assert len(crawler.spider.stats_history) == 2
     assert "third_execution" in crawler.spider.stats_history[0].keys()
     assert "second_execution" in crawler.spider.stats_history[1].keys()
-    crawler.stop()
+    await stop_crawler(crawler)
 
 
+@deferred_f_from_coro_f
 @patch("spidermon.contrib.stats.statscollectors.sc_collections.os.environ.get")
-def test_job_id_added(mock_os_enviorn_get, test_settings, stats_collection):
+async def test_job_id_added(mock_os_enviorn_get, test_settings, stats_collection):
     mock_os_enviorn_get.return_value = "test/test/test"
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     mock_os_enviorn_get.assert_called_with("SCRAPY_JOB", "")
     assert (
@@ -203,12 +218,13 @@ def test_job_id_added(mock_os_enviorn_get, test_settings, stats_collection):
     )
 
 
+@deferred_f_from_coro_f
 @patch("spidermon.contrib.stats.statscollectors.sc_collections.os.environ.get")
-def test_job_id_not_available(mock_os_enviorn_get, test_settings, stats_collection):
+async def test_job_id_not_available(mock_os_enviorn_get, test_settings, stats_collection):
     mock_os_enviorn_get.return_value = None
     crawler = get_crawler(Spider, test_settings)
     crawler.crawl("foo_spider")
-    crawler.stop()
+    await stop_crawler(crawler)
 
     mock_os_enviorn_get.assert_called_with("SCRAPY_JOB", "")
     assert "job_url" not in crawler.spider.stats_history[0]
